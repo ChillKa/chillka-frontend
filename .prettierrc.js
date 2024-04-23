74a43a7217baea609f9bbdea3e280b1a69316582
hongzhu zhao <hongzhu.zhao@kangdainfo.com>
2024-04-23 13:39:32 +0800

    feat: 🎸 把 .prettierrc.json 改成 js 檔案和 tabWidth 寬度修改成2

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'es5',
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 2,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    plugins: ['prettier-plugin-tailwindcss']
+};
