d049634989949d3e516ef4a32479290cf4b481a3
FelixShih <zx8714245@gmail.com>
2024-05-16 00:10:08 +0800

    feat: change to use `EventCard`

--- src/components/RecommendActivity/RecommendActivity.tsx
+++ src/components/RecommendActivity/RecommendActivity.tsx
@@ -5,7 +5,7 @@ interface RecommendActivityProps {}
 
 const RecommendActivity: React.FC<RecommendActivityProps> = () => {
   return (
-    <section className="debug w-full">
+    <section className="w-full">
       <div className="flex w-full items-start justify-between">
         <h1 className="mb-2 text-5xl font-bold leading-10">推薦活動</h1>
         <Button variant="ghost">查看活動</Button>
@@ -24,12 +24,30 @@ const RecommendActivity: React.FC<RecommendActivityProps> = () => {
           pricing={100}
           discount={70}
         />
-        <div id="event-card" className="bg-red h-[564px] w-[416px]">
-          card2
-        </div>
-        <div id="event-card" className="bg-red h-[564px] w-[416px]">
-          card3
-        </div>
+        <EventCard
+          title="城市探險尋寶"
+          cover="https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4"
+          description="穿梭於城市的街巷間，透過尋寶遊戲，探索隱藏的文化景點與歷史故事。"
+          startTime="2024.7.6"
+          endTime="2024.6.30"
+          attendeeCount={999}
+          location="台北市 / 中正區"
+          organizer="臺灣健康教育暨長"
+          pricing={100}
+          discount={30}
+        />
+        <EventCard
+          title="極光露營體驗"
+          cover="https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4"
+          description="在極光閃耀的北極圈，搭建帳篷，與夥伴們共享營火溫暖，目睹極光的壯麗奇景。"
+          startTime="2024.7.6"
+          endTime="2024.6.30"
+          attendeeCount={999}
+          location="台北市 / 中正區"
+          organizer="安妮雅喜歡這個"
+          pricing={100}
+          discount={25}
+        />
       </div>
     </section>
   );
