76d52395863f2d9ff26bad9b20b82272825bd262
Po-Cheng Yeh <blp100x@hotmail.com>
2024-05-16 14:33:47 +0800

    feat: update color settings to follow the design guidelines

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ const config = {
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        surface: 'hsl(var(--surface))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
