259bbbc6180e49ddf9d224582eaa7101d57e1781
Po-Cheng Yeh <blp100x@hotmail.com>
2024-05-19 13:40:57 +0800

    fix: category height unit `px` to `rem`

--- src/components/TopCategoryMenu.tsx
+++ src/components/TopCategoryMenu.tsx
@@ -68,7 +68,7 @@ const items: ItemType[] = [
 ];
 
 const CategoryItemRow = ({ rowItems }: { rowItems: ItemType[] }) => (
-  <div className="flex h-40 gap-4 xl:h-[300px]">
+  <div className="flex h-40 gap-4 xl:h-[18.75rem]">
     {rowItems.map((item, index) => (
       <Fragment key={item.text}>
         <CategoryIconButton
