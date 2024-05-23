e7ecc4b63d6e906ce152530caf2be19f144aeebc
Ella Lu <luyizhen93@gmail.com>
2024-05-23 22:56:16 +0800

    refactor: 💡 use typography component

--- src/components/CallToActionSestion/CallToActionSestion.tsx
+++ src/components/CallToActionSestion/CallToActionSestion.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@components/ui/button';
 import { Separator } from '@components/ui/separator';
+import { H1, Small } from '@components/ui/typography';
 import cn from '@lib/utils';
 import Video from './Video';
 
@@ -16,25 +17,25 @@ const CallToActionSestion = ({ className }: CalltoActionSestionProps) => {
       )}
     >
       <div className="absolute left-0 top-0">
-        <h1 className="w-fit bg-surface pl-[13px] pr-4 text-5xl font-bold -tracking-[0.012em] xl:pb-4">
+        <H1 className="w-fit bg-surface pl-[13px] pr-4 xl:pb-4">
           找不到
           <br className="hidden xl:block" />
           心目中的
           <br className="xl:hidden" />
           活動嗎？
-        </h1>
+        </H1>
         <h1 className="w-[360.98px] bg-surface  pb-4 pl-[13px] pr-4 text-5xl font-bold -tracking-[0.012em] xl:-mt-3 xl:w-fit">
           讓你來揪咖！
         </h1>
         <div className="w-fit bg-surface px-4 py-[30px]">
           <Separator className="h-[2px] w-12 bg-primary" />
         </div>
-        <p className="w-fit bg-surface p-4 pb-0 text-sm font-medium leading-6">
+        <Small className="w-fit bg-surface p-4 pb-0 leading-6">
           Your Crew, Your Call
-        </p>
-        <p className="-mt-2 w-fit bg-surface p-4 pt-2 text-sm font-medium leading-6">
+        </Small>
+        <Small className="-mt-2 w-fit bg-surface p-4 pt-2 leading-6">
           Bring Your Own Adventure!
-        </p>
+        </Small>
       </div>
       <Video className="h-[750px] w-full xl:h-[420px] xl:px-[110px]" />
       <div className="absolute bottom-0 right-0 pb-[33px] pl-[31px] pr-[30px] pt-[34px]">
