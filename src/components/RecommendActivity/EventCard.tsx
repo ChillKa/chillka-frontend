4d0fe0542de58226228e8579bf11ff2e77c1726c
FelixShih <zx8714245@gmail.com>
2024-05-25 09:43:53 +0800

    feat: add skelton card

--- src/components/RecommendActivity/EventCard.tsx
+++ src/components/RecommendActivity/EventCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Skeleton } from '@components/ui/skeleton';
 import cn from '@lib/utils';
 import { cva } from 'class-variance-authority';
 import {
@@ -132,4 +133,55 @@ const EventCard = ({
   );
 };
 
+const SkeletonEventCard = () => {
+  return (
+    <div
+      id="skeleton-card"
+      className={cn(
+        'bg-red flex h-[35.25rem] w-full flex-col gap-8',
+        'xl:w-[26rem]'
+      )}
+    >
+      <div className="relative h-[13rem] w-full overflow-hidden">
+        <Skeleton className="absolute left-0 top-0 h-full w-full" />
+      </div>
+
+      <div className="w- flex h-[5.5rem] flex-col gap-4">
+        <Skeleton className="h-6 w-3/4" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+      </div>
+
+      <div className="flex h-[9rem] flex-col justify-between gap-4">
+        <div className="flex justify-start gap-4">
+          <Skeleton className="h-6 w-6" />
+          <Skeleton className="h-6 w-16" />
+          <Skeleton className="h-6 w-32" />
+        </div>
+        <div className="flex justify-start gap-4">
+          <Skeleton className="h-6 w-6" />
+          <Skeleton className="h-6 w-16" />
+          <Skeleton className="h-6 w-32" />
+        </div>
+        <div className="flex justify-start gap-4">
+          <Skeleton className="h-6 w-6" />
+          <Skeleton className="h-6 w-16" />
+          <Skeleton className="h-6 w-32" />
+        </div>
+        <div className="flex justify-start gap-4">
+          <Skeleton className="h-6 w-6" />
+          <Skeleton className="h-6 w-16" />
+          <Skeleton className="h-6 w-32" />
+        </div>
+      </div>
+
+      <div className="flex h-7 items-center justify-start gap-2">
+        <Skeleton className="h-6 w-16" />
+        <Skeleton className="h-6 w-16" />
+      </div>
+    </div>
+  );
+};
+
+export { SkeletonEventCard };
 export default EventCard;
--- src/components/RecommendActivity/RecommendActivity.tsx
+++ src/components/RecommendActivity/RecommendActivity.tsx
@@ -1,9 +1,64 @@
 import { Button } from '@components/ui/button';
 import cn from '@lib/utils';
 import { ArrowUpRight } from 'lucide-react';
-import EventCard from './EventCard';
+import EventCard, { SkeletonEventCard } from './EventCard';
+import { FormatDate } from './RecommendActivity-types';
 
 const RecommendActivity = () => {
+  const result = {
+    status: 'success',
+    data: [
+      {
+        id: 0,
+        title: '夕陽海灘派對夕陽海灘派對夕陽海灘派對',
+        cover:
+          'https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4',
+        description:
+          '在金色夕陽下，與夥伴們一同沙灘狂歡，享受音樂、美食和海浪聲。在金色夕陽下，與夥伴們一同沙灘狂歡，享受音樂、美食和海浪聲。',
+        startTime: '2024.01.01',
+        endTime: '2024.06.30',
+        attendeeCount: 999,
+        location: '台北市 / 信義區',
+        organizer: '台灣蜘蛛人登高社團',
+        pricing: 100,
+        discount: 70,
+        isCollected: true,
+      },
+      {
+        id: 1,
+        title: '城市探險尋寶',
+        cover:
+          'https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4',
+        description:
+          '穿梭於城市的街巷間，透過尋寶遊戲，探索隱藏的文化景點與歷史故事。',
+        startTime: '2024.07.06',
+        endTime: '2024.06.30',
+        attendeeCount: 999,
+        location: '台北市 / 中正區',
+        organizer: '臺灣健康教育暨長',
+        pricing: 100,
+        discount: 30,
+        isCollected: false,
+      },
+      {
+        id: 2,
+        title: '極光露營體驗',
+        cover:
+          'https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4',
+        description:
+          '在極光閃耀的北極圈，搭建帳篷，與夥伴們共享營火溫暖，目睹極光的壯麗奇景。',
+        startTime: '2024.07.06',
+        endTime: '2024.06.30',
+        attendeeCount: 999,
+        location: '台北市 / 中正區',
+        organizer: '安妮雅喜歡這個',
+        pricing: 100,
+        discount: 25,
+        isCollected: false,
+      },
+    ],
+  };
+
   return (
     <section className={cn('w-full p-3', 'xl:w-[81rem] xl:p-0')}>
       <div className="flex w-full items-start justify-between">
@@ -23,45 +78,27 @@ const RecommendActivity = () => {
       </div>
       <hr className="mb-12 mt-12 w-12 border-t-2 border-gray-400" />
       <div className="flex w-full flex-col justify-between space-y-4 xl:flex-row xl:space-y-0">
-        <EventCard
-          title="夕陽海灘派對夕陽海灘派對夕陽海灘派對"
-          cover="https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4"
-          description="在金色夕陽下，與夥伴們一同沙灘狂歡，享受音樂、美食和海浪聲。在金色夕陽下，與夥伴們一同沙灘狂歡，享受音樂、美食和海浪聲。"
-          startTime="2024.01.01"
-          endTime="2024.06.30"
-          attendeeCount={999}
-          location="台北市 / 信義區"
-          organizer="台灣蜘蛛人登高社團"
-          pricing={100}
-          discount={70}
-          isCollected
-        />
-        <EventCard
-          title="城市探險尋寶"
-          cover="https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4"
-          description="穿梭於城市的街巷間，透過尋寶遊戲，探索隱藏的文化景點與歷史故事。"
-          startTime="2024.07.06"
-          endTime="2024.06.30"
-          attendeeCount={999}
-          location="台北市 / 中正區"
-          organizer="臺灣健康教育暨長"
-          pricing={100}
-          discount={30}
-          isCollected={false}
-        />
-        <EventCard
-          title="極光露營體驗"
-          cover="https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4"
-          description="在極光閃耀的北極圈，搭建帳篷，與夥伴們共享營火溫暖，目睹極光的壯麗奇景。"
-          startTime="2024.07.06"
-          endTime="2024.06.30"
-          attendeeCount={999}
-          location="台北市 / 中正區"
-          organizer="安妮雅喜歡這個"
-          pricing={100}
-          discount={25}
-          isCollected={false}
-        />
+        {result.status === 'loading'
+          ? Array.from({ length: 3 }).map((_, index) => {
+              const id = index;
+              return <SkeletonEventCard key={id} />;
+            })
+          : result.data.map((event) => (
+              <EventCard
+                key={event.id}
+                title={event.title}
+                cover={event.cover}
+                description={event.description}
+                startTime={event.startTime as FormatDate<'YY.MM.DD'>}
+                endTime={event.endTime as FormatDate<'YY.MM.DD'>}
+                attendeeCount={event.attendeeCount}
+                location={event.location}
+                organizer={event.organizer}
+                pricing={event.pricing}
+                discount={event.discount}
+                isCollected={event.isCollected}
+              />
+            ))}
         <Button
           variant="outline"
           className={cn(
