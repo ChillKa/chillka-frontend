a0426d3efa4b84405ad3aec0858ba488aeebaabb
Ella Lu <luyizhen93@gmail.com>
2024-05-26 19:01:10 +0800

    fix: 🐛 button hover effect

--- src/components/CallToActionSection/CallToActionSection.tsx
+++ src/components/CallToActionSection/CallToActionSection.tsx
@@ -3,6 +3,7 @@ import { Separator } from '@components/ui/separator';
 import { H1, Small } from '@components/ui/typography';
 import cn from '@lib/utils';
 import Link from 'next/link';
+import CallToActionButtonBackground from './CallToActionButtonBackground';
 import Video from './Video';
 
 type CalltoActionSectionProps = {
@@ -25,7 +26,7 @@ const CallToActionSection = ({ className }: CalltoActionSectionProps) => {
           <br className="xl:hidden" />
           活動嗎？
         </H1>
-        <h1 className="w-[360.98px] bg-surface  pb-4 pl-[13px] pr-4 text-5xl font-bold -tracking-[0.012em] xl:-mt-3 xl:w-fit">
+        <h1 className="w-[360.98px] bg-surface pb-4 pl-[13px] pr-4 text-5xl font-bold -tracking-[0.012em] xl:-mt-3 xl:w-fit">
           讓你來揪咖！
         </h1>
         <div className="w-fit bg-surface px-4 py-8">
@@ -40,8 +41,16 @@ const CallToActionSection = ({ className }: CalltoActionSectionProps) => {
       </div>
       <Video className="h-[750px] w-full xl:h-[420px] xl:px-[110px]" />
       <div className="absolute bottom-0 right-0 pb-[33px] pl-[31px] pr-[30px] pt-[34px]">
-        <Button className="h-[134px] w-[140px] bg-transparent bg-[url('/callToActionButton-surface.svg')] text-base font-medium text-primary transition duration-300 ease-out hover:bg-transparent hover:bg-[url('/callToActionButton-primary.svg')] hover:text-surface">
-          <Link href="/activity">開始揪咖</Link>
+        <Button className="w-ful relative h-full bg-transparent p-0 text-base font-medium text-surface transition duration-300 ease-out hover:bg-transparent hover:text-primary">
+          <div className="absolute left-0 top-0 ">
+            <CallToActionButtonBackground />
+          </div>
+          <Link
+            href="/activity"
+            className="z-10 px-[38px] py-[55px] text-primary hover:text-surface"
+          >
+            開始揪咖
+          </Link>
         </Button>
       </div>
     </section>
