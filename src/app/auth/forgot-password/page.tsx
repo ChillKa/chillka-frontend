81879b8ded8442909ef3f0a7b0c80bcf2e93c026
Ella Lu <luyizhen93@gmail.com>
2024-06-09 08:45:41 +0800

    feat: 🎸 add forgot password page UI

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,5 +1,20 @@
-const Page = () => {
-  return <div>忘記密碼</div>;
+import { Card, CardContent, CardHeader } from '@components/ui/card';
+import { H3 } from '@components/ui/typography';
+import ForgotPasswordForm from '@components/user/ForgotPasswordForm';
+
+const ForgotPasswordPage = () => {
+  return (
+    <section>
+      <Card className="bg-surface">
+        <CardHeader className="text-center">
+          <H3 className="text-primary">忘記密碼</H3>
+        </CardHeader>
+        <CardContent>
+          <ForgotPasswordForm />
+        </CardContent>
+      </Card>
+    </section>
+  );
 };
 
-export default Page;
+export default ForgotPasswordPage;
--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -31,6 +31,14 @@ export const registerFormSchema = z.object({
   displayName: z.string().min(1).max(50),
 });
 
+export const forgotPasswordFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'This field has to be filled.' })
+    .max(50)
+    .email(),
+});
+
 export const userFormSchema = z.object({
   displayName: z
     .string()
