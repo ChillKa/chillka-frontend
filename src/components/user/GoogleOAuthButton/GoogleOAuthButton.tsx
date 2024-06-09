f4ccfa92b26e96133277ed71db43d42d31f4b875
Ella Lu <luyizhen93@gmail.com>
2024-06-09 16:35:00 +0800

    fix: 🐛 modify default button hover color

--- src/components/user/GoogleOAuthButton/GoogleOAuthButton.tsx
+++ src/components/user/GoogleOAuthButton/GoogleOAuthButton.tsx
@@ -25,7 +25,7 @@ const GoogleOAuthButton = ({ action }: GoogleOAuthButtonProps) => {
       <Separator className="my-4 h-[0.0625rem] w-full" />
       <Button
         variant="outline"
-        className="w-full border-primary bg-[#F2F2F2] py-[0.625rem] text-[#1F1F1F]"
+        className="w-full border-primary bg-[#F2F2F2] py-[0.625rem] text-[#1F1F1F] hover:bg-[#F2F2F2]"
         onClick={handleGoogleOAuth}
         disabled={isPending}
       >
