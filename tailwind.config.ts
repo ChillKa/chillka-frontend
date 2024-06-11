b22ff5d129115299c820cda271f36d9f8bba1523
Po-Cheng Yeh <blp100x@hotmail.com>
2024-06-11 17:35:54 +0800

    chore: add new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,6 +31,8 @@ const config = {
         primary: {
           DEFAULT: 'hsl(var(--primary)/<alpha-value>)',
           foreground: 'hsl(var(--primary-foreground)/<alpha-value>)',
+          light: 'hsl(var(--primary-light)/<alpha-value>)',
+          'super-light': 'hsl(var(--primary-super-light)/<alpha-value>)',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary)/<alpha-value>)',
