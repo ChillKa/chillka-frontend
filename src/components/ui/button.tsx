82cc1e3408d039d01f498738c6d52e73daa8ab66
Po-Cheng Yeh <70035368+blp100@users.noreply.github.com>
2024-06-12 22:26:52 +0800

    chore: add shadcn components and modify styles (#55)
    * chore: add shadcm components
    
    * chore: add new colors
    
    * chore: remove swr package
    
    * feat: add shadcn breadcrumb
    
    * chore: add button style
    
    * chore: add input style
    
    * chore: add radio-item style
    
    * chore: remove date-picker
    
    * feat: add locale on calendar
    
    * chore: add `aschild` props in typography

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,6 +17,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        form: 'bg-primary text-white hover:bg-primary/90 rounded-[0.375rem]',
       },
       size: {
         default: 'h-10 px-4 py-2',
