792243c06c555fe4d807315e0da154466acd0780
FelixShih <zx8714245@gmail.com>
2024-06-13 18:39:49 +0800

    feat: create the search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,46 @@
-const Page = () => {
-  return <div />;
+import SearchBar from '@components/SearchBar';
+
+type SearchPageProps = {
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default Page;
+const getSearchFilter = (params: SearchPageProps['searchParams']) => {
+  const allowedParams = [
+    'keyword',
+    'category',
+    'location',
+    'date',
+    'distance',
+    'sort',
+    'limit',
+    'page',
+  ];
+
+  return Object.keys(params).reduce(
+    (acc, key) => {
+      if (allowedParams.includes(key)) {
+        acc[key] = params[key];
+      }
+      return acc;
+    },
+    {} as { [key: string]: string | string[] | undefined }
+  );
+};
+
+const SearchPage = ({ searchParams }: SearchPageProps) => {
+  const filteredParams = getSearchFilter(searchParams);
+  console.log(filteredParams);
+
+  return (
+    <>
+      <section id="search">
+        <SearchBar className="" />
+      </section>
+      <section id="result">
+        <div>Search Section</div>
+      </section>
+    </>
+  );
+};
+
+export default SearchPage;
