b7420689f9993b780c58b99a02615602ca54947f
FelixShih <zx8714245@gmail.com>
2024-06-14 14:04:52 +0800

    feat: add the activity section

--- src/components/SearchBar/AdvancedSearchBar.tsx
+++ src/components/SearchBar/AdvancedSearchBar.tsx
@@ -77,142 +77,175 @@ const AdvancedSearchBar = ({ filteredParams }: AdvancedSearchBarProps) => {
 
   return (
     <Form {...form}>
-      <section
-        className={cn(
-          'z-20 max-w-[81rem] space-y-6 border-t border-primary bg-surface py-6 text-primary'
-        )}
-      >
-        <div className="flex gap-2">
-          <form onSubmit={handleSearchSubmit} className="flex grow">
-            <ActivityField
-              activityKeywords={[
-                {
-                  url: 'https://www.google.com',
-                  keyword: '露營',
-                },
-                {
-                  url: 'https://www.google.com',
-                  keyword: '酒精路跑',
-                },
-                {
-                  url: 'https://www.google.com',
-                  keyword: '奇美',
-                },
-                {
-                  url: 'https://www.google.com',
-                  keyword: '野餐',
-                },
-                {
-                  url: 'https://www.google.com',
-                  keyword: '登山',
-                },
-              ]}
-              activityPictures={[
-                {
-                  thumbnail:
-                    'https://images.unsplash.com/photo-1546484458-6904289cd4f0?q=100&w=416&h=fit&fm=webp',
-                  url: '/',
-                  description: '夕陽海灘派對',
-                },
-                {
-                  thumbnail:
-                    'https://plus.unsplash.com/premium_photo-1663099746128-34ea20ac094d?q=100&w=416&h=fit&fm=webp',
-                  url: '/',
-                  description: '城市探險尋寶',
-                },
-                {
-                  thumbnail:
-                    'https://images.unsplash.com/photo-1525177089949-b1488a0ea5b6?q=100&w=416&h=fit&fm=webp',
-                  url: '/',
-                  description: '極光露營體驗',
-                },
-              ]}
-              isSearchBarMenuOpen={isSearchBarMenuOpen}
-              setIsCategoryMenuOpen={setIsCategoryMenuOpen}
-              setIsLocationMenuOpen={setIsLocationMenuOpen}
-              setIsSearchBarMenuOpen={setIsSearchBarMenuOpen}
-            />
-            <CategoryFieldMenu
-              isCategoryMenuOpen={isCategoryMenuOpen}
-              setIsCategoryMenuOpen={setIsCategoryMenuOpen}
-              categories={[
-                {
-                  icon: TreesIcon,
-                  url: 'https://www.google.com/',
-                  text: '戶外踏青',
-                },
-                {
-                  icon: PartyPopperIcon,
-                  url: '/',
-                  text: '社交活動',
-                },
-                {
-                  icon: HeartIcon,
-                  url: '/',
-                  text: '興趣嗜好',
-                },
-                {
-                  icon: DumbbellIcon,
-                  url: '/',
-                  text: '運動健身',
-                },
-                {
-                  icon: HazeIcon,
-                  url: '/',
-                  text: '健康生活',
-                },
-                {
-                  icon: BotIcon,
-                  url: '/',
-                  text: '科技玩物',
-                },
-                {
-                  icon: PaletteIcon,
-                  url: '/',
-                  text: '藝術文化',
-                },
-                {
-                  icon: Gamepad2Icon,
-                  url: '/',
-                  text: '遊戲',
-                },
-              ]}
-            />
-            <LocationFieldMenu
-              isLocationMenuOpen={isLocationMenuOpen}
-              setIsLocationMenuOpen={setIsLocationMenuOpen}
-              locations={[
-                {
-                  url: '/',
-                  text: '北部',
-                },
-                {
-                  url: '/',
-                  text: '中部',
-                },
-                {
-                  url: '/',
-                  text: '南部',
-                },
-                {
-                  url: '/',
-                  text: '東部',
-                },
-                {
-                  url: '/',
-                  text: '離島',
-                },
-              ]}
-            />
-            <Button
-              type="submit"
-              className="flex h-auto self-auto px-20 text-xl font-bold"
+      <form onSubmit={handleSearchSubmit}>
+        <section className="flex flex-col">
+          <div
+            className={cn(
+              'z-20 max-w-[81rem] space-y-6 border-t border-primary bg-surface pt-6 text-primary',
+              'flex gap-2'
+            )}
+          >
+            <div className="flex grow">
+              <ActivityField
+                activityKeywords={[
+                  {
+                    url: 'https://www.google.com',
+                    keyword: '露營',
+                  },
+                  {
+                    url: 'https://www.google.com',
+                    keyword: '酒精路跑',
+                  },
+                  {
+                    url: 'https://www.google.com',
+                    keyword: '奇美',
+                  },
+                  {
+                    url: 'https://www.google.com',
+                    keyword: '野餐',
+                  },
+                  {
+                    url: 'https://www.google.com',
+                    keyword: '登山',
+                  },
+                ]}
+                activityPictures={[
+                  {
+                    thumbnail:
+                      'https://images.unsplash.com/photo-1546484458-6904289cd4f0?q=100&w=416&h=fit&fm=webp',
+                    url: '/',
+                    description: '夕陽海灘派對',
+                  },
+                  {
+                    thumbnail:
+                      'https://plus.unsplash.com/premium_photo-1663099746128-34ea20ac094d?q=100&w=416&h=fit&fm=webp',
+                    url: '/',
+                    description: '城市探險尋寶',
+                  },
+                  {
+                    thumbnail:
+                      'https://images.unsplash.com/photo-1525177089949-b1488a0ea5b6?q=100&w=416&h=fit&fm=webp',
+                    url: '/',
+                    description: '極光露營體驗',
+                  },
+                ]}
+                isSearchBarMenuOpen={isSearchBarMenuOpen}
+                setIsCategoryMenuOpen={setIsCategoryMenuOpen}
+                setIsLocationMenuOpen={setIsLocationMenuOpen}
+                setIsSearchBarMenuOpen={setIsSearchBarMenuOpen}
+              />
+              <CategoryFieldMenu
+                isCategoryMenuOpen={isCategoryMenuOpen}
+                setIsCategoryMenuOpen={setIsCategoryMenuOpen}
+                categories={[
+                  {
+                    icon: TreesIcon,
+                    url: 'https://www.google.com/',
+                    text: '戶外踏青',
+                  },
+                  {
+                    icon: PartyPopperIcon,
+                    url: '/',
+                    text: '社交活動',
+                  },
+                  {
+                    icon: HeartIcon,
+                    url: '/',
+                    text: '興趣嗜好',
+                  },
+                  {
+                    icon: DumbbellIcon,
+                    url: '/',
+                    text: '運動健身',
+                  },
+                  {
+                    icon: HazeIcon,
+                    url: '/',
+                    text: '健康生活',
+                  },
+                  {
+                    icon: BotIcon,
+                    url: '/',
+                    text: '科技玩物',
+                  },
+                  {
+                    icon: PaletteIcon,
+                    url: '/',
+                    text: '藝術文化',
+                  },
+                  {
+                    icon: Gamepad2Icon,
+                    url: '/',
+                    text: '遊戲',
+                  },
+                ]}
+              />
+              <LocationFieldMenu
+                isLocationMenuOpen={isLocationMenuOpen}
+                setIsLocationMenuOpen={setIsLocationMenuOpen}
+                locations={[
+                  {
+                    url: '/',
+                    text: '北部',
+                  },
+                  {
+                    url: '/',
+                    text: '中部',
+                  },
+                  {
+                    url: '/',
+                    text: '南部',
+                  },
+                  {
+                    url: '/',
+                    text: '東部',
+                  },
+                  {
+                    url: '/',
+                    text: '離島',
+                  },
+                ]}
+              />
+              <Button
+                type="submit"
+                className="flex h-auto self-auto px-20 text-xl font-bold"
+              >
+                搜尋活動
+              </Button>
+            </div>
+          </div>
+          <div
+            className={cn(
+              'z-20 h-[5.5rem] max-w-[8.125rem] space-y-6 bg-surface pb-6 text-primary',
+              'flex gap-2'
+            )}
+          >
+            <div
+              className={cn('mt-[1px] flex grow border-b border-primary py-4')}
             >
-              搜尋活動
-            </Button>
-          </form>
-        </div>
-      </section>
+              <div className="flex w-full flex-row items-center justify-center border-x border-primary px-4">
+                {['活動', '團體'].map((item) => (
+                  <button
+                    type="button"
+                    disabled={item === '團體'}
+                    key={item}
+                    className="h-full w-full space-y-2 border-primary "
+                  >
+                    <p
+                      className={cn(
+                        'font-bold',
+                        item === '團體' ? 'text-muted-foreground' : ''
+                      )}
+                    >
+                      {item}
+                    </p>
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </form>
     </Form>
   );
 };
