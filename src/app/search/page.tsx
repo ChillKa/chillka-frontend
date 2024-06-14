15d9857e17b41656fa8e8bc23d7be76c46bf56f1
FelixShih <zx8714245@gmail.com>
2024-06-14 11:42:43 +0800

    feat: modify the layout display

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -30,7 +30,7 @@ const ActivitiesList = async ({ filteredParams }: ActivitiesListProps) => {
   const result = await getActivitiesByFilter(filteredParams);
 
   return (
-    <div className="flex flex-col gap-2">
+    <>
       {result.map((activity) => (
         <div key={activity.id} className="debug w-[600px]">
           <h1>{activity.name}</h1>
@@ -39,7 +39,7 @@ const ActivitiesList = async ({ filteredParams }: ActivitiesListProps) => {
           </p>
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
@@ -56,9 +56,11 @@ const SearchPage = ({ searchParams }: SearchPageProps) => {
         <AdvancedSearchBar filteredParams={filteredParams} />
       </section>
       <section id="result" className="flex flex-row gap-2">
-        <Suspense fallback={<div>Loading...</div>}>
-          <ActivitiesList filteredParams={filteredParams} />
-        </Suspense>
+        <div className="flex w-[70%] flex-col gap-2">
+          <Suspense fallback={<div>Loading...</div>}>
+            <ActivitiesList filteredParams={filteredParams} />
+          </Suspense>
+        </div>
         <div>
           {/* TODO: get by result */}
           result map section
