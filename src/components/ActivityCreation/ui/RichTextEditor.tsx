0342cca8ae9f10a15e25749e99c86b8476a5f714
Po-Cheng Yeh <blp100x@hotmail.com>
2024-06-17 14:44:12 +0800

    feat: add rich text editor

---
+++ src/components/ActivityCreation/ui/RichTextEditor.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { Separator } from '@components/ui/separator';
+import { Toggle } from '@components/ui/toggle';
+import { EditorContent, useEditor, type Editor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import {
+  BoldIcon,
+  ItalicIcon,
+  ListIcon,
+  ListOrderedIcon,
+  StrikethroughIcon,
+} from 'lucide-react';
+
+const RichTextEditorToolbar = ({ editor }: { editor: Editor }) => {
+  return (
+    <div className="flex flex-row items-center gap-1 rounded-b-[0.375rem] bg-primary-super-light p-1">
+      <Toggle
+        variant="editor"
+        size="sm"
+        pressed={editor.isActive('bold')}
+        onPressedChange={() => editor.chain().focus().toggleBold().run()}
+      >
+        <BoldIcon className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        variant="editor"
+        size="sm"
+        pressed={editor.isActive('italic')}
+        onPressedChange={() => editor.chain().focus().toggleItalic().run()}
+      >
+        <ItalicIcon className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        variant="editor"
+        size="sm"
+        pressed={editor.isActive('strike')}
+        onPressedChange={() => editor.chain().focus().toggleStrike().run()}
+      >
+        <StrikethroughIcon className="h-4 w-4" />
+      </Toggle>
+      <Separator orientation="vertical" className="h-8 w-[1px]" />
+      <Toggle
+        variant="editor"
+        size="sm"
+        pressed={editor.isActive('bulletList')}
+        onPressedChange={() => editor.chain().focus().toggleBulletList().run()}
+      >
+        <ListIcon className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        variant="editor"
+        size="sm"
+        pressed={editor.isActive('orderedList')}
+        onPressedChange={() => editor.chain().focus().toggleOrderedList().run()}
+      >
+        <ListOrderedIcon className="h-4 w-4" />
+      </Toggle>
+    </div>
+  );
+};
+
+const RichTextEditor = () => {
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class:
+          'max-w-[53.5rem] h-60 w-full rounded-t-[0.375rem] border border-primary-super-light bg-white px-3 py-2 border-b-0 ring-offset-background placeholder:text-primary-light focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 overflow-auto',
+      },
+    },
+    extensions: [
+      StarterKit.configure({
+        orderedList: {
+          HTMLAttributes: {
+            class: 'list-decimal pl-4',
+          },
+        },
+        bulletList: {
+          HTMLAttributes: {
+            class: 'list-disc pl-4',
+          },
+        },
+      }),
+    ],
+    content: '<p>請輸入活動說明</p>',
+  });
+
+  return (
+    <div>
+      <EditorContent editor={editor} />
+      {editor ? <RichTextEditorToolbar editor={editor} /> : null}
+    </div>
+  );
+};
+
+export default RichTextEditor;
