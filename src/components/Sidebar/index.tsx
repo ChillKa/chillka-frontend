357c4a300dddc8e6a916cb183ac1806438af631f
zim <qwe1472571@gmail.com>
2024-06-17 13:33:44 +0800

    feat: 🎸 add menu item

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,18 @@
 import {
+  SITEMAP,
+  registerAndLoginList,
+  userList,
+} from '@components/Header/menu';
+import {
   Dialog,
   DialogClose,
   DialogContent,
   DialogTrigger,
 } from '@components/ui/dialog';
+import { Separator } from '@components/ui/separator';
 import { Menu, X } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 
 const defaultAvatar = '/header__defaultAvatar.svg';
@@ -16,9 +23,19 @@ type SidebarProps = {
   onSignOut: () => void;
 };
 
+type List = {
+  name: string;
+  icon?: JSX.Element;
+  url: string;
+};
+
 const Sidebar = ({ isLoggedin, onSignOut }: SidebarProps) => {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(onSignOut);
+
+  const handlePopoverClick = (event: React.MouseEvent) => {
+    const isLinkClick = (event.target as HTMLElement).closest('a');
+    if (isLinkClick) setIsOpen(false);
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -38,7 +55,11 @@ const Sidebar = ({ isLoggedin, onSignOut }: SidebarProps) => {
           height={40}
         />
       </DialogTrigger>
-      <DialogContent hideCloseButton className="h-svh w-svw">
+      <DialogContent
+        hideCloseButton
+        className="block h-svh w-svw"
+        onClickCapture={handlePopoverClick}
+      >
         <DialogClose asChild>
           <button
             type="button"
@@ -46,6 +67,61 @@ const Sidebar = ({ isLoggedin, onSignOut }: SidebarProps) => {
             className="fixed right-[0.75rem] top-[1.5rem] h-[4rem] w-[7rem] rounded-full opacity-0"
           />
         </DialogClose>
+        <div className="no-scrollbar mt-[7rem] h-[calc(100svh-7rem)] overflow-scroll">
+          {isLoggedin ? (
+            <>
+              {userList.map((user: List) => (
+                <Link
+                  className="mb-4 flex justify-between px-[1.5rem] py-[0.375rem] text-xl font-bold hover:bg-primary/[0.03]"
+                  key={user.name}
+                  href={user.url}
+                >
+                  <p>{user.name}</p>
+                  {user.icon && user.icon}
+                </Link>
+              ))}
+            </>
+          ) : (
+            <>
+              {registerAndLoginList.map((list: List) => (
+                <Link
+                  className="mb-4 flex justify-between px-[1.5rem] py-[0.375rem] text-xl font-bold hover:bg-primary/[0.03]"
+                  key={list.name}
+                  href={list.url}
+                >
+                  <p>{list.name}</p>
+                  {list.icon && list.icon}
+                </Link>
+              ))}
+            </>
+          )}
+          <Separator className="mb-4 h-[0.0625rem] bg-primary" />
+          {SITEMAP.map((map: List) => (
+            <Link
+              className="mb-4 flex justify-between px-[1.5rem] py-[0.5rem] text-base/7 hover:bg-primary/[0.03]"
+              key={map.name}
+              href={map.url}
+            >
+              <div>{map.name}</div>
+            </Link>
+          ))}
+          {isLoggedin && (
+            <>
+              <Separator className="h-[0.0625rem] bg-primary" />
+              <Link href="/">
+                <button
+                  type="button"
+                  className="block h-[4.75rem] w-full px-8 py-0 text-start text-base hover:bg-primary/[0.03]"
+                  onClick={() => {
+                    onSignOut?.();
+                  }}
+                >
+                  登出
+                </button>
+              </Link>
+            </>
+          )}
+        </div>
       </DialogContent>
     </Dialog>
   );
