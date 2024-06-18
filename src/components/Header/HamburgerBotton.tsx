4ad70399ce604a04eaea9ab35bea7265cbdbf786
zim <147781652+zim314@users.noreply.github.com>
2024-06-18 19:55:58 +0800

    feat: 🎸 create mobile version of the sidebar (#63)
    * feat: 🎸 create close layout
    
    * feat: 🎸 create basic sidebar layout
    
    * feat: 🎸 add menu item
    
    * feat: 🎸 add hamburger animation
    
    * feat: 🎸 add menu animation
    
    * fix: 🐛 change hamburger position
    
    * feat: 🎸 separate the hamburger component
    
    * feat: 🎸 add Popover animation
    
    * fix: 🐛 add email and notification
    
    * fix: 🐛 change menu hover bouncing out effect

--- src/components/Header/HamburgerBotton.tsx
+++ src/components/Header/HamburgerBotton.tsx
@@ -1,3 +1,4 @@
+import Hamburger from '@components/Hamburger';
 import {
   SITEMAP,
   registerAndLoginList,
@@ -9,10 +10,8 @@ import {
   PopoverTrigger,
 } from '@components/ui/popover';
 import { Separator } from '@components/ui/separator';
-import { Menu, X } from 'lucide-react';
-import Image from 'next/image';
+import { motion, useCycle } from 'framer-motion';
 import Link from 'next/link';
-import { useState } from 'react';
 
 type HamburgerProps = {
   isLoggedin: boolean;
@@ -25,92 +24,147 @@ type List = {
   url: string;
 };
 
-const defaultAvatar = '/header__defaultAvatar.svg';
-const fakeAvatar = '/header__fakeAvatar.svg';
+const menuAnimation = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
 
 const HamburgerBotton = ({ isLoggedin, onSignOut }: HamburgerProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, toggleOpen] = useCycle(false, true);
 
   const handlePopoverClick = (event: React.MouseEvent) => {
     const isLinkClick = (event.target as HTMLElement).closest('a');
-    if (isLinkClick) setIsOpen(false);
+    if (isLinkClick) toggleOpen();
+  };
+
+  const changeIsOpen = (open: boolean) => {
+    if (open !== isOpen) toggleOpen();
   };
 
   return (
-    <Popover open={isOpen} onOpenChange={setIsOpen}>
-      <PopoverTrigger className="mx-[0.1875rem] flex items-center justify-center rounded-full border border-primary p-3 data-[state=open]:mx-0 data-[state=open]:border-4">
-        <div className="p-2">
-          {isOpen ? <X size={24} /> : <Menu size={24} />}
-        </div>
-        <Image
-          className="ml-2"
-          src={isLoggedin ? fakeAvatar : defaultAvatar}
-          alt="user"
-          width={40}
-          height={40}
-        />
+    <Popover open={isOpen} onOpenChange={changeIsOpen}>
+      <PopoverTrigger>
+        <Hamburger isOpen={isOpen} isLoggedin={isLoggedin} />
       </PopoverTrigger>
       <PopoverContent
         onClickCapture={handlePopoverClick}
         className="h-0 w-0 border-none bg-transparent p-0 text-primary"
       >
-        <div
-          className={`no-scrollbar absolute right-[-3.5rem] box-content hidden h-fit max-h-[28.875rem] w-[17rem] overflow-scroll rounded-[2rem] border-4 border-primary bg-surface pt-6 xl:block ${!isLoggedin && 'pb-2'}`}
+        <motion.ul
+          initial="closed"
+          animate={isOpen ? 'open' : 'closed'}
+          variants={{
+            open: {
+              transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+            },
+            closed: {
+              transition: { staggerChildren: 0.05, staggerDirection: -1 },
+            },
+          }}
         >
-          {isLoggedin ? (
-            <>
-              {userList.map((user: List) => (
+          <div
+            className={`no-scrollbar absolute right-[-3.5rem] box-content hidden h-fit max-h-[28.875rem] w-[17rem] overflow-scroll rounded-[2rem] border-4 border-primary bg-surface pt-6 xl:block ${!isLoggedin && 'pb-2'}`}
+          >
+            {isLoggedin ? (
+              <>
+                {userList.map((user: List) => (
+                  <motion.li
+                    variants={menuAnimation}
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    key={user.name}
+                  >
+                    <Link
+                      className="mb-4 flex justify-between px-8 py-[0.625rem] hover:bg-primary/[0.03]"
+                      key={user.name}
+                      href={user.url}
+                    >
+                      <div className="text-xl font-bold">{user.name}</div>
+                      {user.icon && user.icon}
+                    </Link>
+                  </motion.li>
+                ))}
+              </>
+            ) : (
+              <>
+                {registerAndLoginList.map((list: List) => (
+                  <motion.li
+                    variants={menuAnimation}
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    key={list.name}
+                  >
+                    <Link
+                      className="mb-4 flex justify-between px-8 py-[0.625rem] hover:bg-primary/[0.03]"
+                      href={list.url}
+                    >
+                      <div className="text-xl font-bold">{list.name}</div>
+                      {list.icon && list.icon}
+                    </Link>
+                  </motion.li>
+                ))}
+              </>
+            )}
+            <motion.li variants={menuAnimation}>
+              <Separator className="mb-4 h-[0.0625rem] bg-primary" />
+            </motion.li>
+            {SITEMAP.map((map: List) => (
+              <motion.li
+                variants={menuAnimation}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                key={map.name}
+              >
                 <Link
-                  className="mb-4 flex justify-between px-8 py-[0.625rem] hover:bg-primary/[0.03]"
-                  key={user.name}
-                  href={user.url}
+                  className="mb-4 flex justify-between px-8 py-2 hover:bg-primary/[0.03]"
+                  href={map.url}
                 >
-                  <div className="text-xl font-bold">{user.name}</div>
-                  {user.icon && user.icon}
+                  <div className="text-base leading-7">{map.name}</div>
                 </Link>
-              ))}
-            </>
-          ) : (
-            <>
-              {registerAndLoginList.map((list: List) => (
-                <Link
-                  className="mb-4 flex justify-between px-8 py-[0.625rem] hover:bg-primary/[0.03]"
-                  key={list.name}
-                  href={list.url}
+              </motion.li>
+            ))}
+            {isLoggedin && (
+              <>
+                <motion.li
+                  variants={menuAnimation}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
                 >
-                  <div className="text-xl font-bold">{list.name}</div>
-                  {list.icon && list.icon}
-                </Link>
-              ))}
-            </>
-          )}
-          <Separator className="mb-4 h-[0.0625rem] bg-primary" />
-          {SITEMAP.map((map: List) => (
-            <Link
-              className="mb-4 flex justify-between px-8 py-2 hover:bg-primary/[0.03]"
-              key={map.name}
-              href={map.url}
-            >
-              <div className="text-base leading-7">{map.name}</div>
-            </Link>
-          ))}
-          {isLoggedin && (
-            <>
-              <Separator className="h-[0.0625rem] bg-primary" />
-              <Link href="/">
-                <button
-                  type="button"
-                  className="block h-[4.75rem] w-full px-8 py-0 text-start text-base hover:bg-primary/[0.03]"
-                  onClick={() => {
-                    onSignOut?.();
-                  }}
+                  <Separator className="h-[0.0625rem] bg-primary" />
+                </motion.li>
+                <motion.li
+                  variants={menuAnimation}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
                 >
-                  登出
-                </button>
-              </Link>
-            </>
-          )}
-        </div>
+                  <Link href="/">
+                    <button
+                      type="button"
+                      className="block h-[4.75rem] w-full px-8 py-0 text-start text-base hover:bg-primary/[0.03]"
+                      onClick={() => {
+                        onSignOut?.();
+                      }}
+                    >
+                      登出
+                    </button>
+                  </Link>
+                </motion.li>
+              </>
+            )}
+          </div>
+        </motion.ul>
       </PopoverContent>
     </Popover>
   );
