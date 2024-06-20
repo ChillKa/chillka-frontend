1ca08aac2b3a85943b2b6003450361ba63c29737
Po-Cheng Yeh <blp100x@hotmail.com>
2024-06-20 15:20:48 +0800

    feat: add create-activity form schema

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -73,3 +73,76 @@ export const userFormSchema = z.object({
     .min(2, { message: MIN_LENGTH_MESSAGE(2) })
     .max(50, { message: MAX_LENGTH_MESSAGE(50) }),
 });
+
+export const createActivityFormSchema = z.object({
+  name: z.string({ required_error: '請填寫活動名稱', message: 'testing' }),
+  organizer: z.object({
+    profilePicture: z.string().optional(),
+    name: z.string({ required_error: '請填寫主辦方名稱' }),
+    contactName: z.string({
+      required_error: '請填寫聯絡人姓名',
+    }),
+    contactPhone: z.string({
+      required_error: '請填寫聯絡人電話',
+    }),
+    contactEmail: z.string({
+      required_error: '請填寫聯絡人電子郵件',
+    }),
+    websiteName: z.string().optional(),
+    websiteURL: z.string().optional(),
+  }),
+  cover: z.array(z.string({ required_error: '請至少上傳一張圖片' })),
+  thumbnail: z.string({ required_error: '請上傳一張縮圖' }),
+  startDateTime: z.date().optional(),
+  fromToday: z.boolean({
+    required_error: 'FromToday is required',
+  }),
+  endDateTime: z.date().optional(),
+  noEndDate: z.boolean({
+    required_error: 'NoEndDate is required',
+  }),
+  category: z.string({ required_error: '請選擇活動類型' }),
+  type: z.string({ required_error: '請選擇活動形式' }),
+  link: z.string().optional(),
+  location: z.string().optional(),
+  address: z.string().optional(),
+  summary: z.string({ required_error: '請填寫活動摘要' }),
+  details: z
+    .string({ required_error: '請填寫活動詳情' })
+    .min(20, { message: '請至少填寫20個字以上' }),
+  isPrivate: z.boolean({ required_error: 'IsPrivate is required' }),
+  displayRemainingTickets: z.boolean({
+    required_error: 'DisplayRemainingTickets is required',
+  }),
+  isRecurring: z.boolean({ required_error: 'IsRecurring is required' }),
+  recurring: z
+    .object({
+      period: z.string(),
+      week: z.string(),
+      day: z.string(),
+    })
+    .optional(),
+  status: z.string({ required_error: 'Status is required' }).optional(),
+  tickets: z.array(
+    z.object({
+      _id: z.string().optional(), // optional for edit activity
+      name: z.string({ required_error: '請填寫票卷名稱' }),
+      price: z.number({ required_error: '請輸入票卷價格' }),
+      startDateTime: z.date().optional(),
+      fromToday: z.boolean({ required_error: 'FromToday is required' }),
+      endDateTime: z.date().optional(),
+      noEndDate: z.boolean({ required_error: 'NoEndDate is required' }),
+      participantCapacity: z.number({
+        required_error: '請選擇參與人數',
+      }),
+      unlimitedQuantity: z.boolean({
+        required_error: 'UnlimitedQuantity is required',
+      }),
+      purchaseLimit: z.number().optional(),
+      description: z.string().optional(),
+      purchaseDuplicate: z.boolean().optional(),
+      ticketStatus: z.string().optional(),
+      serialNumber: z.string().optional(),
+    })
+  ),
+});
