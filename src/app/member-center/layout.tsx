ad99925195034b3f32a69d9382cf6e3a3583abf4
zim <qwe1472571@gmail.com>
2024-06-21 01:42:34 +0800

    feat: 🎸 add animation to navbar

--- src/app/member-center/layout.tsx
+++ src/app/member-center/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Navbar from '@components/Navbar';
 
 type MemberCenterLayoutProps = {
@@ -6,11 +8,9 @@ type MemberCenterLayoutProps = {
 
 const MemberCenterLayout = ({ children }: MemberCenterLayoutProps) => {
   return (
-    <div className="flex items-center justify-center px-[0.75rem] py-[4.75rem]">
-      <div className="flex w-full max-w-[81rem]">
-        <Navbar className="mr-[8.375rem] hidden w-[19.125rem] xl:block" />
-        <div className="debug flex-1">{children}</div>
-      </div>
+    <div className="m-auto my-[4.75rem] flex w-full max-w-[81rem] px-[0.75rem]">
+      <Navbar className="mr-[8.375rem] hidden w-[19.125rem] xl:block" />
+      <div className="debug flex-1">{children}</div>
     </div>
   );
 };
