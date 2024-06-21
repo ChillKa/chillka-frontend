ef918e9acf60f2bdbe5af4fa5724caaf28a97139
FelixShih <zx8714245@gmail.com>
2024-06-21 10:58:33 +0800

    feat: create the SearchProvider and it encapsulate shadcn form

---
+++ src/components/SearchBar/SearchProvider.tsx
@@ -0,0 +1,85 @@
+import { FormField, FormItem } from '@components/ui/form';
+import { ReactNode, createContext, useContext } from 'react';
+import {
+  DefaultValues,
+  FieldPath,
+  FieldValues,
+  FormProvider,
+  RegisterOptions,
+  Resolver,
+  SubmitHandler,
+  UseFormReturn,
+  useForm,
+} from 'react-hook-form';
+
+export type SearchProviderProps<T extends FieldValues> = {
+  children: ReactNode;
+  defaultValues: DefaultValues<T>;
+  onSubmit: SubmitHandler<T>;
+  resolver?: Resolver<T>;
+};
+
+type SearchContextProps<T extends FieldValues> = UseFormReturn<T>;
+
+const SearchContext = createContext<SearchContextProps<any> | undefined>(
+  undefined
+);
+
+const SearchProvider = <T extends FieldValues>({
+  children,
+  defaultValues,
+  onSubmit,
+  resolver,
+}: SearchProviderProps<T>) => {
+  const methods = useForm<T>({ defaultValues, resolver });
+
+  return (
+    <SearchContext.Provider value={methods}>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+      </FormProvider>
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearch = <T extends FieldValues>(): SearchContextProps<T> => {
+  const context = useContext(SearchContext);
+  if (!context) {
+    throw new Error('useSearch must be used within a SearchProvider');
+  }
+  return context;
+};
+
+export type SearchFieldProps<T extends FieldValues> = {
+  name: FieldPath<T>;
+  rules?: RegisterOptions;
+  children: (props: {
+    value: any;
+    onChange: (value: any) => void;
+  }) => ReactNode;
+};
+
+export const SearchField = <T extends FieldValues>({
+  name,
+  rules,
+  children,
+}: SearchFieldProps<T>) => {
+  const methods = useSearch<T>();
+
+  return (
+    <FormField
+      control={methods.control}
+      name={name}
+      rules={rules}
+      render={({ field }) => {
+        return (
+          <FormItem>
+            {children({ value: field.value, onChange: field.onChange })}
+          </FormItem>
+        );
+      }}
+    />
+  );
+};
+
+export default SearchProvider;
