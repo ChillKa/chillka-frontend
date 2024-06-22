c5b1c6e29f3a22e9f9511c3e2ae2b8b7089af706
FelixShih <zx8714245@gmail.com>
2024-06-22 09:57:23 +0800

    fix: the date not checked problem

--- src/components/SearchBar/fields/DateFieldMenu.tsx
+++ src/components/SearchBar/fields/DateFieldMenu.tsx
@@ -254,7 +254,7 @@ export const AdvancedDateMobileField = ({
                         key={date.text}
                         id={`radio-${date.text}`}
                         className="flex h-fit items-center justify-between gap-2.5 bg-surface px-4 py-2.5 transition-colors duration-300 ease-out hover:bg-primary/[0.03]"
-                        value={date.text}
+                        value={customDate ? format(customDate, 'PPP') : ''}
                       >
                         <div className="flex w-full items-center justify-between">
                           {customDate ? (
