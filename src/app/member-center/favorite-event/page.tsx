91443cc05b936cc730c61fd4aad59933911d76e3
zim <qwe1472571@gmail.com>
2024-06-26 14:18:44 +0800

    feat: 🎸 creat favorite event layout

--- src/app/member-center/favorite-event/page.tsx
+++ src/app/member-center/favorite-event/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+
 const FavoriteEvent = () => {
-  return <div>收藏活動 FavoriteEvent</div>;
+  return (
+    <div className="relative text-primary">
+      <h1 className="h-[5rem] border-b-[0.0625rem] text-5xl/none font-bold xl:h-[6.25rem]">
+        收藏活動
+      </h1>
+    </div>
+  );
 };
 
 export default FavoriteEvent;
