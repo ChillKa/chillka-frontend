d44f6cac47f9724efdd59786e2ae557e3c3fecf3
zim <147781652+zim314@users.noreply.github.com>
2024-06-26 13:44:00 +0800

    Refactor:hide non display pages (#76)
    * refactor: 💡 change redirect after login success
    
    * refactor: 💡 hidden manage-event page

--- src/components/Navbar/fixedData.ts
+++ src/components/Navbar/fixedData.ts
@@ -3,10 +3,10 @@ export const aboutEventist = [
     title: '查詢票券',
     url: '/member-center/ticket-inquiry',
   },
-  {
-    title: '管理活動',
-    url: '/member-center/manage-event',
-  },
+  // {
+  //   title: '管理活動',
+  //   url: '/member-center/manage-event',
+  // },
   {
     title: '收藏活動',
     url: '/member-center/favorite-event',
