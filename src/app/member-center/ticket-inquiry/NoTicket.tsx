ecb341e2e352f8ca2127bb0ec6fd2a046c4a7d6c
zim <qwe1472571@gmail.com>
2024-06-27 17:47:20 +0800

    style: 💄 splitting the ticket inquiry layout

---
+++ src/app/member-center/ticket-inquiry/NoTicket.tsx
@@ -0,0 +1,9 @@
+const NoTicket = () => {
+  return (
+    <div className="flex h-[5rem] w-full items-center justify-center text-xl font-bold">
+      no tickets
+    </div>
+  );
+};
+
+export default NoTicket;
--- src/app/member-center/ticket-inquiry/page.tsx
+++ src/app/member-center/ticket-inquiry/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
+import NoTicket from '@app/member-center/ticket-inquiry/NoTicket';
 import QRCodePopUp from '@components/QRCodePopUp';
 import SortOrder from '@components/SortOrder';
 import TicketPopUp from '@components/TicketPopUp';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/ui/tabs';
 import useWindowSize from '@hooks/use-window-size';
 import { useState } from 'react';
 import fakerData from './fakerData';
 
 const TicketInquiry = () => {
   const [sort, setSort] = useState('1');
-  const [canUse, setCanUse] = useState(true);
-
   const { width } = useWindowSize();
 
   const handleSort = (value: string) => setSort(value);
@@ -25,53 +25,57 @@ const TicketInquiry = () => {
           changeState={handleSort}
         />
       </div>
-      <div>
-        <div className="flex border-b-[0.0625rem] font-bold xl:border-b-0">
-          <button
-            className={`mr-[1.5rem] pt-6 ${canUse ? 'border-b-4 pb-5' : 'pb-6'} `}
-            type="button"
-            onClick={() => setCanUse(true)}
+      <Tabs defaultValue="usableTicket">
+        <TabsList className="h-fit w-full justify-start border-b-[0.0625rem] bg-surface p-0 xl:border-b-0">
+          <TabsTrigger
+            className="mr-6 bg-surface px-0 py-6 text-xl font-bold text-primary data-[state=active]:border-b-4 data-[state=active]:pb-5 data-[state=active]:text-primary"
+            value="usableTicket"
           >
             可使用
-          </button>
-          <button
-            className={`pt-6 ${canUse ? 'pb-6' : 'border-b-4 pb-5'} `}
-            type="button"
-            onClick={() => setCanUse(false)}
+          </TabsTrigger>
+          <TabsTrigger
+            className="bg-surface px-0 py-6 text-xl font-bold text-primary data-[state=active]:border-b-4 data-[state=active]:pb-5 data-[state=active]:text-primary"
+            value="unusableTicket"
           >
             已使用或過期
-          </button>
-        </div>
+          </TabsTrigger>
+        </TabsList>
         <ul className="hidden grid-cols-[7fr_2fr_2fr_2fr] border-y-[0.0625rem] py-4 text-xl font-bold xl:grid">
           <li className="text-left">活動名稱</li>
           <li className="text-center">數量</li>
           <li className="text-center">使用期限</li>
           <li className="text-center">開啟票券</li>
         </ul>
-      </div>
-      {canUse &&
-        fakerData.map((ticker) => {
-          return (
-            <TicketPopUp key={ticker.orderNumber}>
-              <div className="block grid-cols-[7fr_2fr_2fr_2fr] py-4 text-xl font-bold xl:grid">
-                <h3 className="mb-[1.25rem] text-left xl:mb-0 xl:font-bold">
-                  {ticker.title}
-                </h3>
-                <p className="mr-2 inline-block bg-primary px-2 py-1 text-xs/5 font-medium text-white xl:mr-0 xl:flex xl:items-center xl:justify-center xl:bg-surface xl:p-0 xl:text-xl xl:font-bold xl:text-primary">
-                  {width > 1366 ? ticker.quantity : `數量：${ticker.quantity}`}
-                </p>
-                <p className="inline-block bg-primary px-2 py-1 text-xs/5 font-medium text-white xl:flex xl:items-center xl:justify-center xl:bg-surface xl:p-0 xl:text-xl xl:font-bold xl:text-primary">
-                  {width > 1366
-                    ? ticker.expirationDate
-                    : `使用期限：${ticker.expirationDate}`}
-                </p>
-                <div className="xl:flex xl:items-center xl:justify-center">
-                  <QRCodePopUp />
+        <TabsContent value="usableTicket">
+          {fakerData.map((ticker) => {
+            return (
+              <TicketPopUp key={ticker.orderNumber}>
+                <div className="block grid-cols-[7fr_2fr_2fr_2fr] py-4 text-xl font-bold xl:grid">
+                  <h3 className="mb-[1.25rem] text-left xl:mb-0 xl:font-bold">
+                    {ticker.title}
+                  </h3>
+                  <p className="mr-2 inline-block bg-primary px-2 py-1 text-xs/5 font-medium text-white xl:mr-0 xl:flex xl:items-center xl:justify-center xl:bg-surface xl:p-0 xl:text-xl xl:font-bold xl:text-primary">
+                    {width > 1366
+                      ? ticker.quantity
+                      : `數量：${ticker.quantity}`}
+                  </p>
+                  <p className="inline-block bg-primary px-2 py-1 text-xs/5 font-medium text-white xl:flex xl:items-center xl:justify-center xl:bg-surface xl:p-0 xl:text-xl xl:font-bold xl:text-primary">
+                    {width > 1366
+                      ? ticker.expirationDate
+                      : `使用期限：${ticker.expirationDate}`}
+                  </p>
+                  <div className="xl:flex xl:items-center xl:justify-center">
+                    <QRCodePopUp />
+                  </div>
                 </div>
-              </div>
-            </TicketPopUp>
-          );
-        })}
+              </TicketPopUp>
+            );
+          })}
+        </TabsContent>
+        <TabsContent value="unusableTicket">
+          <NoTicket />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
