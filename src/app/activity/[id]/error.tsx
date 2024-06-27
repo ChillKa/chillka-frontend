10ec4fd797cbc295b4e78593ca2ee7620347cd7c
Ella Lu <luyizhen93@gmail.com>
2024-06-27 20:07:07 +0800

    feat: 🎸 implement error handling for activity page

--- src/app/activity/[id]/error.tsx
+++ src/app/activity/[id]/error.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import { H3 } from '@components/ui/typography';
 
-const ErrorContent = () => {
+const Error = () => {
   return (
     <div className="flex w-full items-center justify-center py-56 text-primary">
       <H3>很抱歉，找不到此活動。</H3>
@@ -8,4 +10,4 @@ const ErrorContent = () => {
   );
 };
 
-export default ErrorContent;
+export default Error;
