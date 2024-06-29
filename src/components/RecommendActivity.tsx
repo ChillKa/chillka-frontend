68a4ec71638b407d530438ee5a8d985d47aa83ab
Felix Shih <25448040+fufuShih@users.noreply.github.com>
2024-06-29 21:35:44 +0800

    Revert "feat: change to use backend api with nearby and recommend activity (#83)" (#86)
    This reverts commit f39eaa7f86a0ad9468a5f5ca93d1d3bbcaa4091b.

--- src/components/RecommendActivity.tsx
+++ src/components/RecommendActivity.tsx
@@ -1,4 +1,3 @@
-import { fetchRecommendedActivity } from '@action/activity';
 import EventCard, {
   FormatDate,
   SkeletonEventCard,
@@ -6,19 +5,68 @@ import EventCard, {
 import { Button } from '@components/ui/button';
 import { H1 } from '@components/ui/typography';
 import cn from '@lib/utils';
-import { format } from 'date-fns';
 import { ArrowUpRight } from 'lucide-react';
-import Link from 'next/link';
-import { Suspense } from 'react';
-import ErrorBoundarySection from './error/ErrorBoundarySection';
+import ErrorBoudarySection from './error/ErrorBoudarySection';
 
 type RecommendActivityProps = {
   className: string;
 };
 
-const RecommendActivity = async ({ className }: RecommendActivityProps) => {
-  const data = await fetchRecommendedActivity();
-  const { activities } = data;
+const RecommendActivity = ({ className }: RecommendActivityProps) => {
+  const result = {
+    status: 'success',
+    data: [
+      {
+        id: 0,
+        title: '夕陽海灘派對夕陽海灘派對夕陽海灘派對',
+        cover:
+          'https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4',
+        description:
+          '在金色夕陽下，與夥伴們一同沙灘狂歡，享受音樂、美食和海浪聲。在金色夕陽下，與夥伴們一同沙灘狂歡，享受音樂、美食和海浪聲。',
+        startTime: '2024.01.01',
+        endTime: '2024.06.30',
+        attendeeCount: 999,
+        location: '台北市 / 信義區',
+        organizer: '台灣蜘蛛人登高社團',
+        pricing: 100,
+        discount: 70,
+        isCollected: true,
+      },
+      {
+        id: 1,
+        title: '城市探險尋寶',
+        cover:
+          'https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4',
+        description:
+          '穿梭於城市的街巷間，透過尋寶遊戲，探索隱藏的文化景點與歷史故事。',
+        startTime: '2024.07.06',
+        endTime: '2024.06.30',
+        attendeeCount: 999,
+        location: '台北市 / 中正區',
+        organizer: '臺灣健康教育暨長',
+        pricing: 100,
+        discount: 30,
+        isCollected: false,
+      },
+      {
+        id: 2,
+        title: '極光露營體驗',
+        cover:
+          'https://fastly.picsum.photos/id/495/200/200.jpg?hmac=WzrKoNNBWVnlSjTRFVRlUyZghnLUBZJXeXdHNugLsQ4',
+        description:
+          '在極光閃耀的北極圈，搭建帳篷，與夥伴們共享營火溫暖，目睹極光的壯麗奇景。',
+        startTime: '2024.07.06',
+        endTime: '2024.06.30',
+        attendeeCount: 999,
+        location: '台北市 / 中正區',
+        organizer:
+          '安妮雅喜歡這個安妮雅喜歡這個安妮雅喜歡這個安妮雅喜歡這個安妮雅喜歡這個',
+        pricing: 100,
+        discount: 25,
+        isCollected: false,
+      },
+    ],
+  };
 
   return (
     <section
@@ -31,74 +79,58 @@ const RecommendActivity = async ({ className }: RecommendActivityProps) => {
     >
       <div className="flex w-full items-start justify-between">
         <H1>推薦活動</H1>
-        <Link href="/search">
-          <button
-            type="button"
-            className={cn(
-              'relative hidden h-12 w-24 justify-between px-0 pb-4 pt-2 font-medium',
-              'after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:border-t after:bg-primary',
-              'after:transition-colors after:duration-300 after:ease-out hover:after:border-primary',
-              'xl:flex'
-            )}
-          >
-            查看更多
-            <ArrowUpRight />
-          </button>
-        </Link>
+        <button
+          type="button"
+          className={cn(
+            'relative hidden h-12 w-24 justify-between px-0 pb-4 pt-2 font-medium',
+            'after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:border-t after:bg-primary',
+            'after:transition-colors after:duration-300 after:ease-out hover:after:border-primary',
+            'xl:flex'
+          )}
+        >
+          查看更多
+          <ArrowUpRight />
+        </button>
       </div>
       <hr className="mb-12 mt-12 w-12 border-t-2 border-primary" />
       <div className="flex w-full flex-col justify-between space-y-12 xl:flex-row xl:space-y-0">
-        <ErrorBoundarySection>
-          <Suspense
-            fallback={
-              <>
-                {Array.from({ length: 3 }).map((_, index) => {
-                  const id = index;
-                  return <SkeletonEventCard key={id} />;
-                })}
-              </>
-            }
-          >
-            {activities.map((activity) => (
-              <EventCard
-                className="w-[26rem]"
-                key={activity._id}
-                link={activity._id}
-                title={activity.name}
-                cover={activity.thumbnail}
-                description={activity.summary}
-                startTime={
-                  format(
-                    new Date(activity.startDateTime),
-                    'MM.dd'
-                  ) as FormatDate<'YY.MM.DD'>
-                }
-                endTime={format(new Date(), 'MM.dd') as FormatDate<'YY.MM.DD'>}
-                attendeeCount={activity.participantNumber}
-                location={activity.location}
-                organizer={activity.organizerName ?? 'Unknown Organizer'}
-                pricing={activity.ticketPrice[0].price}
-                discount={activity?.discount ?? 0}
-                isCollected={activity?.collected ?? false}
-              />
-            ))}
-          </Suspense>
-        </ErrorBoundarySection>
-        <Link href="/search">
-          <Button
-            variant="outline"
-            className={cn(
-              'flex h-14 w-full items-center justify-center gap-4',
-              'border border-primary px-8 py-4',
-              'text-base font-medium text-primary',
-              'xl:hidden',
-              'transition-colors hover:bg-primary hover:fill-surface hover:text-surface'
-            )}
-          >
-            查看更多推薦活動
-            <ArrowUpRight size={16} />
-          </Button>
-        </Link>
+        <ErrorBoudarySection>
+          {result.status === 'loading'
+            ? Array.from({ length: 3 }).map((_, index) => {
+                const id = index;
+                return <SkeletonEventCard key={id} />;
+              })
+            : result.data.map((event) => (
+                <EventCard
+                  key={event.id}
+                  link="123" // FIXME: change to use event link
+                  title={event.title}
+                  cover={event.cover}
+                  description={event.description}
+                  startTime={event.startTime as FormatDate<'YY.MM.DD'>}
+                  endTime={event.endTime as FormatDate<'YY.MM.DD'>}
+                  attendeeCount={event.attendeeCount}
+                  location={event.location}
+                  organizer={event.organizer}
+                  pricing={event.pricing}
+                  discount={event.discount}
+                  isCollected={event.isCollected}
+                />
+              ))}
+        </ErrorBoudarySection>
+        <Button
+          variant="outline"
+          className={cn(
+            'flex h-14 w-full items-center justify-center gap-4',
+            'border border-primary px-8 py-4',
+            'text-base font-medium text-primary',
+            'xl:hidden',
+            'transition-colors hover:bg-primary hover:fill-surface hover:text-surface'
+          )}
+        >
+          查看更多推薦活動
+          <ArrowUpRight size={16} />
+        </Button>
       </div>
     </section>
   );
