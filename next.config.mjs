07cbdfda3f2c79f942e2c2ad5dcee707670fa21c
FelixShih <zx8714245@gmail.com>
2024-06-29 17:04:50 +0800

    feat: modify the image config and domain

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,11 @@ const nextConfig = {
       process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
   },
   images: {
+    domains: ['unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**.unsplash.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'unsplash.com',
         port: '',
       },
       {
