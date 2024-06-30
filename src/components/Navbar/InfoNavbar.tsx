47030f22adf498b073914b4aab293e894c078e6a
Ella Lu <luyizhen93@gmail.com>
2024-06-30 14:04:02 +0800

    refactor: 💡 enhance use-scroll-spy hook

--- src/components/Navbar/InfoNavbar.tsx
+++ src/components/Navbar/InfoNavbar.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import useScrollspy from '@hooks/use-scroll-spy';
 import cn from '@lib/utils';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
 
 type NavbarItemType = {
   name: string;
@@ -16,53 +16,22 @@ type InfoNavbarProps = {
 };
 
 const InfoNavbar = ({ className, data }: InfoNavbarProps) => {
-  const [activeSection, setActiveSection] = useState<string>(data[0].id);
-  const disableScrollHandlingRef = useRef(false);
+  const ids = data.map((item) => item.id);
+  const headerHeight = 144;
+  const activeId = useScrollspy(ids, headerHeight);
 
-  useEffect(() => {
-    const sectionOffsets: Record<string, number> = {};
-    data.forEach((section) => {
-      const element = document.getElementById(section.id);
-      if (element) {
-        sectionOffsets[section.id] = element.offsetTop;
-      }
-    });
-
-    const handleScroll = () => {
-      if (disableScrollHandlingRef.current) return;
-      const scrollPosition = window.scrollY;
-
-      data.forEach((section) => {
-        const offset = sectionOffsets[section.id];
-        const element = document.getElementById(section.id);
-
-        if (
-          offset &&
-          element &&
-          offset <= scrollPosition &&
-          offset + element.offsetHeight > scrollPosition
-        ) {
-          setActiveSection(section.id);
-        }
-      });
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [data]);
-
-  const handleClick = (id: string) => {
-    disableScrollHandlingRef.current = true;
-
+  const handleClick = (
+    id: string,
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    e.preventDefault();
     const section = document.getElementById(id);
-    section?.scrollIntoView({ behavior: 'smooth' });
-
-    setActiveSection(id);
-    setTimeout(() => {
-      disableScrollHandlingRef.current = false;
-    }, 1000);
+    if (section) {
+      const yOffset = -headerHeight;
+      const yPosition =
+        section.getBoundingClientRect().top + window.scrollY + yOffset;
+      window.scrollTo({ top: yPosition, behavior: 'smooth' });
+    }
   };
 
   return (
@@ -72,14 +41,14 @@ const InfoNavbar = ({ className, data }: InfoNavbarProps) => {
       <ul className="sticky top-12 p-4">
         {data.map((section) => (
           <motion.li
-            className={`mb-6 text-2xl font-bold hover:bg-primary/[0.03] ${activeSection === section.id ? 'border-l-4 border-primary pl-[1.25rem]' : 'pl-6'}`}
+            className={`mb-6 text-2xl font-bold hover:bg-primary/[0.03] ${activeId === section.id ? 'border-l-4 border-primary pl-[1.25rem]' : 'pl-6'}`}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
             key={section.id}
           >
             <Link
               href={`#${section.id}`}
-              onClick={() => handleClick(section.id)}
+              onClick={(e) => handleClick(section.id, e)}
             >
               {section.name}
             </Link>
