1cb5f80891decd4740601c0f0a5ca049abfe58e2
Ella Lu <luyizhen93@gmail.com>
2024-06-30 14:49:35 +0800

    fix: 🐛 correct and update privacy policy page content

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -7,28 +7,36 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@components/ui/breadcrumb';
-import { H2, H3, P } from '@components/ui/typography';
+import { H2, H3, List, P } from '@components/ui/typography';
 
 const TITLE_OF_CONTENT = [
   {
-    name: '授權協議',
-    id: 'authorization-agreement',
+    name: '隱私權保護政策的適用範圍',
+    id: 'scope-of-privacy-policy',
   },
   {
-    name: '使用條件',
-    id: 'terms-of-use',
+    name: '個人資料的蒐集、處理及利用方式',
+    id: 'collection-processing-utilization-of-personal-data',
   },
   {
-    name: '隱私政策',
-    id: 'privacy-policy',
+    name: '資料之保護',
+    id: 'data-protection',
   },
   {
-    name: '責任限制',
-    id: 'limitation-of-liability',
+    name: '網站對外的相關連結',
+    id: 'external-links',
   },
   {
-    name: '爭議解決',
-    id: 'dispute-resolution',
+    name: '與第三人共用個人資料之政策',
+    id: 'policy-on-sharing-personal-data-with-third-parties',
+  },
+  {
+    name: 'Cookie 之使用',
+    id: 'use-of-cookies',
+  },
+  {
+    name: '隱私權保護政策之修正',
+    id: 'modifications-to-privacy-policy',
   },
 ];
 
@@ -57,56 +65,107 @@ const Page = () => {
           <div>
             <H2 className="mb-2">隱私權政策</H2>
             <P>
-              「chillka 揪咖」網站使用條款 歡迎訪問「chillka
-              揪咖」網站（網址：https://chillka-frontend.onrender.com/）。請在使用本網站服務之前仔細閱讀以下條款和條件。進入或使用本網站即表示您同意遵守以下條款和條件。如果您不同意這些條款，請停止使用本網站服務。
+              非常歡迎您光臨「chillka
+              揪咖」（以下簡稱本網站），為了讓您能夠安心的使用本網站的各項服務與資訊，特此向您說明本網站的隱私權保護政策，以保障您的權益，請您詳閱下列內容：
             </P>
           </div>
           <div>
-            <H3 id="authorization-agreement" className="mb-2">
-              一、授權協議
+            <H3 id="scope-of-privacy-policy" className="mb-2">
+              一、隱私權保護政策的適用範圍
             </H3>
             <P>
-              這個網站是由「chillka
-              揪咖」運營。網站上的所有內容（包括文字、圖像、視頻和其他材料）受版權和其他知識產權法律保護。未經「chillka
-              揪咖」明確授權，嚴禁複製、修改、重新發布或分發本網站的內容。
+              隱私權保護政策內容，包括本網站如何處理在您使用網站服務時蒐集到的個人識別資料。隱私權保護政策不適用於本網站以外的相關連結網站，也不適用於非本網站所委託或參與管理的人員。
             </P>
           </div>
           <div>
-            <H3 id="terms-of-use" className="mb-2">
-              二、使用條件
+            <H3
+              id="collection-processing-utilization-of-personal-data"
+              className="mb-2"
+            >
+              二、個人資料的蒐集、處理及利用方式
             </H3>
             <P>
-              您使用本網站的風險自負。我們不對因使用本網站而導致的損失或損害承擔任何責任。使用本網站時，請遵守當地法律法規。我們保留隨時更改或終止本網站服務的權利，恕不另行通知。
+              （一）當您洽辦本網站業務或參與本網站活動時，我們將視業務或活動性質請您提供必要的個人資料，且原則上於蒐集個人資料之特定目的必要範圍內處理或利用您的個人資料。
+            </P>
+            <P>
+              （二）本網站在您使用意見回饋、電子報、問卷調查等互動性功能時，會保留您所提供的姓名、電子郵件地址、聯絡方式及使用時間等。
+            </P>
+            <P>
+              （三）於一般瀏覽時，伺服器會自行記錄相關行徑，包括您使用連線設備的
+              IP
+              位址、使用時間、使用的瀏覽器、瀏覽及點選資料記錄等，做為我們增進網站服務的參考依據，此記錄為內部應用，絕不對外公布。
+            </P>
+            <P>
+              （四）為提供精確的服務，我們會將收集的問卷調查內容進行統計與分析，分析結果之統計數據或說明文字呈現，除供內部研究外，我們會視需要公佈統計數據及說明文字，但不涉及特定個人之資料。
             </P>
           </div>
           <div>
-            <H3 id="privacy-policy" className="mb-2">
-              三、隱私政策
+            <H3 id="data-protection" className="mb-2">
+              三、 資料之保護
             </H3>
             <P>
-              我們尊重用戶的隱私權。您使用本網站即表示同意我們按照我們的隱私政策處理個人信息。請閱讀我們的隱私政策，了解我們如何收集、使用和共享您的信息。
+              （一）本網站主機均設有防火牆、防毒系統等相關的各項資訊安全設備及必要的安全防護措施，加以保護網站及您的個人資料採用嚴格的保護措施，只由經過授權的人員才能接觸您的個人資料，相關處理人員皆簽有保密合約，如有違反保密義務者，將會受到相關的法律處分。
+            </P>
+            <P>
+              （二）如因業務需要有必要委託本網站相關單位提供服務時，本網站亦會嚴格要求其遵守保密義務，並且採取必要檢查程序以確定其將確實遵守。
             </P>
           </div>
           <div>
-            <H3 id="limitation-of-liability" className="mb-2">
-              四、責任限制
+            <H3 id="external-links" className="mb-2">
+              四、 網站對外的相關連結
             </H3>
             <P>
-              在法律允許的範圍內，我們不對因使用本網站而導致的任何直接或間接損失承擔責任。我們不擔保本網站將不受干擾、及時、安全或無錯誤運行。
+              本網站的網頁提供其他網站的網路連結，您也可經由本網站所提供的連結，點選進入其他網站。但該連結網站不適用本網站的隱私權保護政策，您必須參考該連結網站中的隱私權保護政策。
             </P>
           </div>
           <div>
-            <H3 id="dispute-resolution" className="mb-2">
-              五、爭議解決
+            <H3
+              id="policy-on-sharing-personal-data-with-third-parties"
+              className="mb-2"
+            >
+              五、與第三人共用個人資料之政策
             </H3>
             <P>
-              本使用條款受台灣法律管轄。任何因本網站服務而引起的爭議應首先通過友好協商解決。如果協商失敗，雙方同意接受台灣法院的專屬管轄權。
+              （一）本網站絕不會提供、交換、出租或出售任何您
+              的個人資料給其他個人、團體、私人企業或公務機關，但有法律依據者，不在此限。
+            </P>
+            <P>（二）前項但書之情形包括但不限於下列各款：</P>
+            <List className="my-3 list-decimal">
+              <li>經由您同意。</li>
+              <li>法律明文規定。</li>
+              <li>為維護國家安全或增進公共利益所必要。</li>
+              <li>為免除您生命、身體、自由或財產上之危險。</li>
+              <li>
+                公務機關或學術研究機構基於公共利益為統計或學術研究而有必要，且資料經過提供者處理後或經蒐集者依其揭露方式無從識別特定之當事人。
+              </li>
+              <li>
+                當您在本網站的行為，違反本網站的服務條款或可能損害或妨礙本網站權益或導致任何人遭受損害時，經本網站研析揭露您的個人資料是為了辨識、聯絡或採取法律行動所必要者。
+              </li>
+              <li>有利於您的權益。</li>
+            </List>
+            <P>
+              （三）本網站委託廠商協助蒐集、處理或利用您的個人資料時，將對委外廠商或個人善盡監督管理之責。
             </P>
           </div>
-          <P>
-            謝謝您閱讀「chillka
-            揪咖」網站的使用條款。如果您有任何疑問或疑慮，請通過網站上提供的聯絡方式與我們聯繫。
-          </P>
+          <div>
+            <H3 id="use-of-cookies" className="mb-2">
+              六、 Cookie 之使用
+            </H3>
+            <P>
+              為了提供您最佳的服務，本網站會在您的電腦中放置並取用我們的Cookie，若您不願接受
+              Cookie
+              的寫入，您可在您使用的瀏覽器功能項中設定隱私權等級為高，即可拒絕
+              Cookie 的寫入，但可能會導至網站某些功能無法正常執行 。
+            </P>
+          </div>
+          <div>
+            <H3 id="modifications-to-privacy-policy" className="mb-2">
+              七、 隱私權保護政策之修正
+            </H3>
+            <P>
+              本網站隱私權保護政策將因應需求隨時進行修正，修正後的條款將刊登於網站上。
+            </P>
+          </div>
         </div>
       </div>
     </div>
