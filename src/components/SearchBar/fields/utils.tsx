bb4dc2f4efe03f5dca83cedc4f76ea896c974261
FelixShih <zx8714245@gmail.com>
2024-07-02 07:41:18 +0800

    feat: modify the search thumbnail

--- src/components/SearchBar/fields/utils.tsx
+++ src/components/SearchBar/fields/utils.tsx
@@ -11,6 +11,7 @@ import {
 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 import { z } from 'zod';
 
 export const locations = [
@@ -198,12 +199,20 @@ export const ActivityResultThumbnail = ({
   thumbnail,
   description,
 }: ActivityResultThumbnailProps) => {
+  const [imageSrc, setImageSrc] = useState(thumbnail ?? '/default.webp');
+
   return (
     <Link href={link ?? '/search'}>
       <div className="min-w-fit space-y-2">
         <Image
-          src={thumbnail ?? '/default.webp'}
+          src={imageSrc}
           alt={description ?? 'Thumbnail default description'}
+          placeholder="blur"
+          blurDataURL="/loading.webp"
+          onLoadingComplete={(result) => {
+            if (result.naturalWidth === 0) setImageSrc('/default.webp');
+          }}
+          onError={() => setImageSrc('/default.webp')}
           width={200}
           height={100}
           className="h-[6.25rem] w-[12.5rem] object-cover"
