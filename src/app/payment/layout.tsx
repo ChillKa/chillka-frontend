b1e53b2711043bf9b761da237004505a4e711da5
FelixShih <zx8714245@gmail.com>
2024-07-03 17:59:41 +0800

    fix: the breadcrumb hydration problem

--- src/app/payment/layout.tsx
+++ src/app/payment/layout.tsx
@@ -3,6 +3,7 @@
 import {
   Breadcrumb,
   BreadcrumbItem,
+  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
@@ -12,27 +13,40 @@ import { usePathname } from 'next/navigation';
 const PaymentLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
-  const breadcrumbItems = [
-    { href: '/payment/select-tickets', label: '選擇票券' },
-    { href: '/payment/fill-info', label: '填寫資料' },
-    { href: '/payment/complete', label: '訂票完成' },
-  ];
-
   return (
     <section className="mx-auto flex max-w-[81rem] flex-col gap-1">
       <div className="w-full">
         <Breadcrumb>
           <BreadcrumbList>
-            {breadcrumbItems.map((item, index) => (
-              <BreadcrumbItem key={item.href}>
-                {pathname === item.href ? (
-                  <BreadcrumbPage>{item.label}</BreadcrumbPage>
-                ) : (
-                  item.label
-                )}
-                {index < breadcrumbItems.length - 1 && <BreadcrumbSeparator />}
-              </BreadcrumbItem>
-            ))}
+            <BreadcrumbItem>
+              {pathname === '/payment/select-tickets' ? (
+                <BreadcrumbPage>選擇票券</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <p>選擇票券</p>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              {pathname === '/payment/fill-info' ? (
+                <BreadcrumbPage>填寫資料</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <p>填寫資料</p>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              {pathname === '/payment/complete' ? (
+                <BreadcrumbPage>訂票完成</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <p>訂票完成</p>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
       </div>
