2e32d217a825efc0a91516ac586f7d68647ace63
Ella Lu <luyizhen93@gmail.com>
2024-07-03 23:01:52 +0800

    feat: 🎸 add animation to email and notification buttons

--- src/components/Header/EmailButton.tsx
+++ src/components/Header/EmailButton.tsx
@@ -5,10 +5,10 @@ import {
 } from '@components/ui/popover';
 import { Separator } from '@components/ui/separator';
 import { H3, H4, P, Small } from '@components/ui/typography';
+import { motion, useCycle } from 'framer-motion';
 import { Mail } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
 
 const fakeAvatar = '/header__fakeAvatar.svg';
 
@@ -45,16 +45,37 @@ const fakeData = [
   },
 ];
 
+const menuAnimation = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
 const EmailButton = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, toggleOpen] = useCycle(false, true);
 
   const handlePopoverClick = (event: React.MouseEvent) => {
     const isLinkClick = (event.target as HTMLElement).closest('a');
-    if (isLinkClick) setIsOpen(false);
+    if (isLinkClick) toggleOpen();
+  };
+
+  const changeIsOpen = (open: boolean) => {
+    if (open !== isOpen) toggleOpen();
   };
 
   return (
-    <Popover open={isOpen} onOpenChange={setIsOpen}>
+    <Popover open={isOpen} onOpenChange={changeIsOpen}>
       <PopoverTrigger className="ml-[0.1875ren] mr-[0.8125rem] rounded-full border border-primary p-5 data-[state=open]:ml-0 data-[state=open]:mr-[0.625rem] data-[state=open]:border-4">
         <Mail size={24} />
       </PopoverTrigger>
@@ -64,28 +85,50 @@ const EmailButton = () => {
       >
         <div className="absolute right-[-1.875rem] box-content w-96 rounded-[2rem] border-4 border-primary bg-surface pt-6 ">
           <H3 className="mb-2 p-2 px-8">信箱</H3>
-          {fakeData.map(
-            (message, i) =>
-              i < 4 && (
-                <div
-                  className={`flex items-center px-8 py-2 ${i !== 3 ? 'mb-2' : 'mb-4'}`}
-                  key={message.content}
-                >
-                  <Image
-                    className="mr-4"
-                    src={message.avatar}
-                    alt="avatar"
-                    width={40}
-                    height={40}
-                  />
-                  <div>
-                    <H4 className="mb-2">{message.user}</H4>
-                    <P className="mb-2 line-clamp-1 h-7">{message.content}</P>
-                    <Small>{message.time}</Small>
-                  </div>
-                </div>
-              )
-          )}
+          <motion.div
+            initial="closed"
+            animate={isOpen ? 'open' : 'closed'}
+            variants={{
+              open: {
+                transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+              },
+              closed: {
+                transition: { staggerChildren: 0.05, staggerDirection: -1 },
+              },
+            }}
+          >
+            {fakeData.map(
+              (message, i) =>
+                i < 4 && (
+                  <motion.div
+                    variants={menuAnimation}
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    key={message.user}
+                  >
+                    <div
+                      className={`flex cursor-pointer items-center px-8 py-2 hover:bg-primary/[0.03]  ${i !== 3 ? 'mb-2' : 'mb-4'}`}
+                      key={message.content}
+                    >
+                      <Image
+                        className="mr-4"
+                        src={message.avatar}
+                        alt="avatar"
+                        width={40}
+                        height={40}
+                      />
+                      <div>
+                        <H4 className="mb-2">{message.user}</H4>
+                        <P className="mb-2 line-clamp-1 h-7">
+                          {message.content}
+                        </P>
+                        <Small>{message.time}</Small>
+                      </div>
+                    </div>
+                  </motion.div>
+                )
+            )}
+          </motion.div>
           <Separator className="h-[0.0625rem] bg-primary" />
           <Link
             href="/member-center/email"
--- src/components/Header/NotificationButton.tsx
+++ src/components/Header/NotificationButton.tsx
@@ -5,9 +5,9 @@ import {
 } from '@components/ui/popover';
 import { Separator } from '@components/ui/separator';
 import { H3, P, Small } from '@components/ui/typography';
+import { motion, useCycle } from 'framer-motion';
 import { Bell, Lightbulb } from 'lucide-react';
 import Link from 'next/link';
-import { useState } from 'react';
 
 const fakeData = [
   {
@@ -37,16 +37,37 @@ const fakeData = [
   },
 ];
 
+const menuAnimation = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
 const NotificationButton = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, toggleOpen] = useCycle(false, true);
 
   const handlePopoverClick = (event: React.MouseEvent) => {
     const isLinkClick = (event.target as HTMLElement).closest('a');
-    if (isLinkClick) setIsOpen(false);
+    if (isLinkClick) toggleOpen();
+  };
+
+  const changeIsOpen = (open: boolean) => {
+    if (open !== isOpen) toggleOpen();
   };
 
   return (
-    <Popover open={isOpen} onOpenChange={setIsOpen}>
+    <Popover open={isOpen} onOpenChange={changeIsOpen}>
       <PopoverTrigger className="ml-[0.1875rem] mr-[0.8125rem] rounded-full border border-primary p-5 data-[state=open]:ml-0 data-[state=open]:mr-[0.625rem] data-[state=open]:border-4">
         <Bell size={24} />
       </PopoverTrigger>
@@ -56,25 +77,45 @@ const NotificationButton = () => {
       >
         <div className="absolute right-[-1.875rem] box-content w-96 rounded-[2rem] border-4 border-primary bg-surface pt-6 ">
           <H3 className="mb-2 p-2 px-8">通知</H3>
-          {fakeData.map(
-            (message, i) =>
-              i < 4 && (
-                <div
-                  className={`flex items-center px-8 py-2 ${i !== 3 ? 'mb-2' : 'mb-4'}`}
-                  key={message.content}
-                >
-                  <div className="mr-4 rounded-full bg-[#403E3D] p-2">
-                    <Lightbulb className="text-white" size={24} />
-                  </div>
-                  <div>
-                    <P className="line-clamp-2 max-h-14 font-bold">
-                      {message.content}
-                    </P>
-                    <Small className="mt-2">{message.time}</Small>
-                  </div>
-                </div>
-              )
-          )}
+          <motion.div
+            initial="closed"
+            animate={isOpen ? 'open' : 'closed'}
+            variants={{
+              open: {
+                transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+              },
+              closed: {
+                transition: { staggerChildren: 0.05, staggerDirection: -1 },
+              },
+            }}
+          >
+            {fakeData.map(
+              (message, i) =>
+                i < 4 && (
+                  <motion.div
+                    variants={menuAnimation}
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    key={message.content}
+                  >
+                    <div
+                      className={`flex cursor-pointer items-center px-8 py-2 hover:bg-primary/[0.03] ${i !== 3 ? 'mb-2' : 'mb-4'}`}
+                      key={message.content}
+                    >
+                      <div className="mr-4 rounded-full bg-[#403E3D] p-2">
+                        <Lightbulb className="text-white" size={24} />
+                      </div>
+                      <div>
+                        <P className="line-clamp-2 max-h-14 font-bold">
+                          {message.content}
+                        </P>
+                        <Small className="mt-2">{message.time}</Small>
+                      </div>
+                    </div>
+                  </motion.div>
+                )
+            )}
+          </motion.div>
           <Separator className="h-[0.0625rem] bg-primary" />
           <Link
             href="/"
