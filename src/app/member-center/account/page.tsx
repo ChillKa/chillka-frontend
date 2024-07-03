21432b81f994fd3913008f3d9e41fd2f57c2cc32
Po-Cheng Yeh <blp100x@hotmail.com>
2024-07-03 22:51:46 +0800

    feat: change background color

--- src/app/member-center/account/page.tsx
+++ src/app/member-center/account/page.tsx
@@ -1,19 +1,16 @@
 import { fetchMe } from '@action/user';
-import { Card } from '@components/ui/card';
 import UserProfileForm from '@components/user/UserProfileForm';
 
 const Account = async () => {
   const result = await fetchMe();
 
   return (
-    <section className="flex w-full justify-center p-2">
-      <Card className="mt-2 w-full p-5">
-        {result.status === 'success' ? (
-          <UserProfileForm defaultData={result.data} />
-        ) : (
-          <div>{result.message}</div>
-        )}
-      </Card>
+    <section className="flex w-full justify-center text-primary">
+      {result.status === 'success' ? (
+        <UserProfileForm defaultData={result.data} />
+      ) : (
+        <div>{result.message}</div>
+      )}
     </section>
   );
 };
