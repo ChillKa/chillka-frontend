b23807e1718b3ee3c70a7d83d773fbfda05a9555
Po-Cheng Yeh <blp100x@hotmail.com>
2024-07-05 16:48:35 +0800

    fix: largest contentful paint(LCP) warning

--- src/components/LogoButton/LogoButton.tsx
+++ src/components/LogoButton/LogoButton.tsx
@@ -3,7 +3,13 @@ import Link from 'next/link';
 
 const LogoButton = () => (
   <Link href="/">
-    <Image width={140} height={32} src="/logo.svg" alt="chillka logo" />
+    <Image
+      width={140}
+      height={32}
+      src="/logo.svg"
+      priority
+      alt="chillka logo"
+    />
   </Link>
 );
 
