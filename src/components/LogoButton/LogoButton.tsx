2cae499e971316ab7f4cae98d0eff34cf0139eb8
Po-Cheng Yeh <blp100x@hotmail.com>
2024-07-05 16:45:24 +0800

    refactor: extract logo button from header and create individual component

--- src/components/LogoButton/LogoButton.tsx
+++ src/components/LogoButton/LogoButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const LogoButton = () => (
   <Link href="/">
-    <Image width={140} height={32} src="/logo.svg" alt="chillka" />
+    <Image width={140} height={32} src="/logo.svg" alt="chillka logo" />
   </Link>
 );
 
