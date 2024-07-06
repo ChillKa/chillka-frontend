55bd1839200e1eb8647972f4446f856a82a83186
FelixShih <zx8714245@gmail.com>
2024-07-06 13:33:54 +0800

    feat: modify and create wrapped function

--- src/components/hoc/WithErrorBoundaryAndSuspense.tsx
+++ src/components/hoc/WithErrorBoundaryAndSuspense.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ErrorBoundarySection from '@components/error/ErrorBoundarySection';
-import React, { Suspense } from 'react';
+import React, { ComponentType, Suspense } from 'react';
 
 type WithErrorBoundaryAndSuspenseProps = {
   fallback?: React.ReactNode;
@@ -14,13 +14,27 @@ const WithErrorBoundaryAndSuspense = ({
 }: WithErrorBoundaryAndSuspenseProps) => {
   return (
     <ErrorBoundarySection>
-      <Suspense
-        fallback={fallback || <div className="h-fit w-full">Loading...</div>}
-      >
-        {children}
-      </Suspense>
+      <Suspense fallback={fallback}>{children}</Suspense>
     </ErrorBoundarySection>
   );
+};
+
+export const withErrorBoundaryAndSuspense = <P extends object>(
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Component: ComponentType<P>,
+  fallback?: React.ReactNode
+) => {
+  return function errorBoundaryAndSuspense(props: P) {
+    return (
+      <ErrorBoundarySection>
+        <Suspense
+          fallback={fallback || <div className="h-fit w-full">Loading...</div>}
+        >
+          <Component {...props} />
+        </Suspense>
+      </ErrorBoundarySection>
+    );
+  };
 };
 
 export default WithErrorBoundaryAndSuspense;
