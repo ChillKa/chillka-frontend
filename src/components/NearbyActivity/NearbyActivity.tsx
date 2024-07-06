01ec2cfc9b7cafcdce42d1ebbdb1f6c43c904bab
FelixShih <zx8714245@gmail.com>
2024-07-06 13:52:40 +0800

    feat: modify the NearbyActivity

---
+++ src/components/NearbyActivity/NearbyActivity.tsx
@@ -0,0 +1,80 @@
+import { getActivitiesByFilter } from '@action/activity';
+import { Button } from '@components/ui/button';
+import { H1 } from '@components/ui/typography';
+import cn from '@lib/utils';
+import { ArrowUpRight } from 'lucide-react';
+import Link from 'next/link';
+import NearbyActivityContent from './NearbyActivityContent';
+
+type NearbyActivityProps = {
+  className: string;
+};
+
+const NearbyActivity = async ({ className }: NearbyActivityProps) => {
+  const data = await getActivitiesByFilter(
+    {
+      lat: '121.5598',
+      lng: '25.09108',
+      limit: '6',
+    },
+    { cache: 'no-store' }
+  );
+
+  const { activities = [] } = data;
+
+  return (
+    <section
+      className={cn(
+        'mx-auto',
+        'w-full p-[12.5px] text-primary',
+        'xl:w-[81rem] xl:p-0',
+        className
+      )}
+    >
+      <div className="flex w-full items-start justify-between">
+        <H1>附近活動</H1>
+        <Link href="/search">
+          <button
+            type="button"
+            className={cn(
+              'relative hidden h-12 w-24 justify-between px-0 pb-4 pt-2 font-medium',
+              'after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:border-t after:bg-primary',
+              'after:transition-colors after:duration-300 after:ease-out hover:after:border-primary',
+              'xl:flex'
+            )}
+          >
+            查看更多
+            <ArrowUpRight />
+          </button>
+        </Link>
+      </div>
+      <hr className="mb-12 mt-12 w-12 border-t-2 border-primary" />
+      <div
+        className={cn(
+          'flex w-full flex-col justify-between space-y-12',
+          'xl:flex-row xl:flex-wrap xl:gap-6 xl:gap-y-12 xl:space-y-0'
+        )}
+      >
+        <NearbyActivityContent activities={activities} />
+
+        <Link href="/search">
+          <Button
+            variant="outline"
+            className={cn(
+              'flex h-14 w-full items-center justify-center gap-4',
+              'border border-primary px-8 py-4',
+              'text-base font-medium text-primary',
+              'xl:hidden',
+              'transition-colors hover:bg-primary hover:fill-surface hover:text-surface'
+            )}
+          >
+            查看更多附近活動
+            <ArrowUpRight size={16} />
+          </Button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default NearbyActivity;
---
+++ src/components/NearbyActivity/NearbyActivityContent.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { Activity } from '@action/activity';
+import EventCard, {
+  FormatDate,
+  SkeletonEventCard,
+} from '@components/EventCard';
+import { withErrorBoundaryAndSuspense } from '@components/hoc/WithErrorBoundaryAndSuspense';
+import useMediaQuery from '@hooks/use-media-query';
+import { format } from 'date-fns';
+
+type NearbyActivityContentProps = {
+  activities: Activity[];
+};
+const NearbyActivityContent = ({ activities }: NearbyActivityContentProps) => {
+  const { matches: isDefault } = useMediaQuery();
+  const eventsToShow = isDefault ? activities.slice(0, 3) : activities;
+  return (
+    <>
+      {eventsToShow.map((activity) => (
+        <EventCard
+          key={activity._id}
+          className="xl:w-[26rem]"
+          link={activity._id}
+          title={activity?.name}
+          cover={activity?.thumbnail}
+          summary={activity?.summary}
+          startTime={
+            format(
+              new Date(activity.startDateTime),
+              'MM.dd'
+            ) as FormatDate<'YY.MM.DD'>
+          }
+          endTime={
+            format(
+              new Date(activity.endDateTime),
+              'MM.dd'
+            ) as FormatDate<'YY.MM.DD'>
+          }
+          attendeeCount={
+            activity?.totalParticipantCapacity != null &&
+            activity?.remainingTickets != null &&
+            !Number.isNaN(
+              activity.totalParticipantCapacity - activity.remainingTickets
+            )
+              ? activity.totalParticipantCapacity - activity.remainingTickets
+              : 0
+          }
+          location={activity?.type === '線下' ? activity?.address : '線上'}
+          organizer={activity.organizer?.contactName ?? '未知舉辦者'}
+          pricing={activity?.ticketPrice[0].price} // FIXME: change to use ticket price
+          discount={activity?.discount}
+          isCollected={activity?.collected}
+        />
+      ))}
+    </>
+  );
+};
+
+const fallback = (
+  <>
+    {Array.from({ length: 3 }).map((_, index) => {
+      const num = index;
+      return <SkeletonEventCard key={num} />;
+    })}
+  </>
+);
+
+export default withErrorBoundaryAndSuspense(NearbyActivityContent, fallback);
