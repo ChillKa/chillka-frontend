ee54e2cce350032062eb619654ca258f4115fabb
YI-ZHEN , LU <70134552+ellallu0903@users.noreply.github.com>
2024-07-07 18:23:45 +0800

    fix: 🐛 adjust table overflow-x style (#122)

--- src/app/member-center/layout.tsx
+++ src/app/member-center/layout.tsx
@@ -8,7 +8,7 @@ const MemberCenterLayout = ({ children }: MemberCenterLayoutProps) => {
   return (
     <div className="m-auto my-[1.5rem] flex w-full max-w-[81rem] px-[0.75rem] xl:my-[4.75rem]">
       <Navbar className="mr-[8.375rem] hidden w-[19.125rem] xl:block" />
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 overflow-x-auto">{children}</div>
     </div>
   );
 };
