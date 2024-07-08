c43938a0857d9a54074a056eabb939346b5d0c0d
Po-Cheng Yeh <blp100x@hotmail.com>
2024-07-08 22:19:56 +0800

    feat: tweak textarea style

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex min-h-20 w-full rounded-[0.375rem] border border-primary-super-light  bg-white px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex min-h-20 w-full rounded-[0.375rem] border  border-primary-super-light bg-white px-3 py-2 text-sm text-primary ring-offset-background placeholder:text-primary-light focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring  focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-80',
           className
         )}
         ref={ref}
