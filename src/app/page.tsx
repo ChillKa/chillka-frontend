cc59394a01e31f3c0551f4be74a1001d30531c7c
FelixShih <zx8714245@gmail.com>
2024-07-09 08:06:00 +0800

    feat: check the problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CallToActionSection from '@components/CallToActionSection';
-import CommentSection from '@components/CommentSection';
 import HeroSection from '@components/HeroSection';
+import NearbyActivity from '@components/NearbyActivity';
+import RecommendActivity from '@components/RecommendActivity';
 import SearchBar from '@components/SearchBar';
 import TopCategoryMenu from '@components/TopCategoryMenu';
 
@@ -11,11 +12,11 @@ const Home = () => {
     <>
       <HeroSection className="mx-auto xl:mt-14" />
       <SearchBar className="mx-auto xl:mt-2" />
-      {/* <NearbyActivity className="mx-auto mt-24 xl:mt-36" /> */}
+      <NearbyActivity className="mx-auto mt-24 xl:mt-36" />
       <TopCategoryMenu className="mx-auto mt-24 xl:mt-36" />
-      {/* <RecommendActivity className="mx-auto mt-24 xl:mt-36" /> */}
+      <RecommendActivity className="mx-auto mt-24 xl:mt-36" />
       <CallToActionSection className="mx-auto mt-24 xl:mt-36" />
-      <CommentSection className="mx-auto my-24 xl:my-36" />
+      {/* <CommentSection className="mx-auto my-24 xl:my-36" /> */}
     </>
   );
 };
