01a01a1927b3f0c40b85819019c30ec688a4987e
FelixShih <zx8714245@gmail.com>
2024-07-09 15:49:16 +0800

    feat: setup the ticket only select one type

--- src/app/payment/utils/SelectTicketsSection.tsx
+++ src/app/payment/utils/SelectTicketsSection.tsx
@@ -6,6 +6,7 @@ import { Card } from '@components/ui/card';
 import { H3, H4 } from '@components/ui/typography';
 import { formatActivityTime } from '@lib/dateUtils';
 import cn from '@lib/utils';
+import { isAfter } from 'date-fns';
 import {
   CalendarDays,
   MapPin,
@@ -34,6 +35,10 @@ const SelectTicketsSection = ({
   const handleTicketChange = (ticketId: string, change: number) => {
     setSelectedTickets((prev) => {
       const newCount = Math.max(0, (prev[ticketId] || 0) + change);
+      if (newCount === 0) {
+        const { [ticketId]: _, ...rest } = prev;
+        return rest;
+      }
       return { ...prev, [ticketId]: newCount };
     });
   };
@@ -53,6 +58,10 @@ const SelectTicketsSection = ({
 
     router.push(`/payment/${activityId}/fill-info?${searchParams.toString()}`);
   };
+
+  const isAnyTicketSelected = Object.values(selectedTickets).some(
+    (quantity) => quantity > 0
+  );
 
   return (
     <>
@@ -106,7 +115,7 @@ const SelectTicketsSection = ({
       </section>
       <section id="ticket-section" className="flex flex-col gap-2">
         <div className="mb-2 flex flex-row items-center justify-between">
-          <H4>請選擇票券</H4>
+          <H4>請選擇一種票券</H4>
           <div className="flex flex-row items-center gap-2">
             <div id="total-amount">Total: ${totalAmount}</div>
             <Button variant="default" onClick={handleNextStep}>
@@ -115,16 +124,25 @@ const SelectTicketsSection = ({
           </div>
         </div>
         {data.tickets.map((ticket) => {
+          const isDisabled =
+            isAnyTicketSelected && !selectedTickets[ticket._id];
           return (
             <Card
               key={ticket._id}
               id="ticket"
-              className="flex w-full flex-row items-center justify-between gap-3 rounded-xl p-1"
+              className={cn(
+                'flex w-full flex-row items-center justify-between gap-3 rounded-xl p-1',
+                isDisabled && 'bg-gray-200 opacity-50'
+              )}
             >
               <div className="flex max-w-[70%] flex-col gap-2">
                 <H4>{ticket.name}</H4>
                 <p>
-                  {ticket.startDateTime} 至 {ticket.endDateTime}
+                  {formatActivityTime(
+                    ticket.startDateTime,
+                    ticket.endDateTime,
+                    isAfter(new Date('2100-01-01'), ticket.endDateTime)
+                  )}
                 </p>
                 <p>{ticket.description}</p>
               </div>
