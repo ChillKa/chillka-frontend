a779439041e10c82f1099b34f1521e4b63a118f6
Ella Lu <luyizhen93@gmail.com>
2024-07-11 23:22:19 +0800

    feat: 🎸 format all prices

--- src/components/EventCard/SearchResultEventCard.tsx
+++ src/components/EventCard/SearchResultEventCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { H3 } from '@components/ui/typography';
+import { formatPrice } from '@lib/fomatPrice';
 import cn from '@lib/utils';
 import Link from 'next/link';
 import { HTMLAttributes, useMemo, useRef } from 'react';
@@ -126,7 +127,7 @@ const SearchResultEventCard = ({
                 <span className="text-lg font-bold">
                   {nearestTicket.price === 0
                     ? '免費'
-                    : `NT$${nearestTicket.price}`}
+                    : `NT$${formatPrice(nearestTicket.price)}`}
                 </span>
                 {nearestTicket.price > 0 && discountLabel(discount)}
               </>
