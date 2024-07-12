ba5e18ef487e901531b8c05255af66633b494a1e
Felix Shih <25448040+fufuShih@users.noreply.github.com>
2024-07-12 23:01:04 +0800

    feat: add the recommend in complete payment successful page (#153)
    * feat: add the recommend section in complete payment
    
    * feat: add handle the long text case

--- src/app/payment/utils/PaymentSuccessSection.tsx
+++ src/app/payment/utils/PaymentSuccessSection.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { RecommendedActivityFetchState } from '@action/activity';
 import { Button } from '@components/ui/button';
 import { Card } from '@components/ui/card';
+import { Separator } from '@components/ui/separator';
 import { H2, H4, P, Small } from '@components/ui/typography';
 import { useAuthContext } from '@store/AuthProvider/AuthProvider';
-import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
@@ -14,19 +16,19 @@ type PaymentSuccessSectionProps = {
   activityId?: string;
   orderId?: string;
   message?: string;
+  activities?: RecommendedActivityFetchState['activities'];
 };
 
 const PaymentSuccessSection = ({
   activityId,
   orderId,
   message: _message,
+  activities = [],
 }: PaymentSuccessSectionProps) => {
   const router = useRouter();
   const { isLoggedin } = useAuthContext();
   const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
   const [showConfetti, setShowConfetti] = useState(true);
-  const [countdown, setCountdown] = useState(10);
-  const [isRedirecting, setIsRedirecting] = useState(false);
 
   useEffect(() => {
     const updateWindowSize = () => {
@@ -35,30 +37,15 @@ const PaymentSuccessSection = ({
     updateWindowSize();
     window.addEventListener('resize', updateWindowSize);
 
-    const countdownInterval = setInterval(() => {
-      setCountdown((prevCount) => {
-        if (prevCount === 1) {
-          clearInterval(countdownInterval);
-          setIsRedirecting(true);
-          setTimeout(() => {
-            router.push(activityId ? `/activity/${activityId}` : '/');
-          }, 1000); // 給加載圖標一點顯示時間
-          return 0;
-        }
-        return prevCount - 1;
-      });
-    }, 1000);
-
     const confettiTimer = setTimeout(() => {
       setShowConfetti(false);
     }, 10000);
 
     return () => {
       window.removeEventListener('resize', updateWindowSize);
-      clearInterval(countdownInterval);
       clearTimeout(confettiTimer);
     };
-  }, [activityId, router]);
+  }, []);
 
   return (
     <>
@@ -70,7 +57,7 @@ const PaymentSuccessSection = ({
           numberOfPieces={200}
         />
       )}
-      <Card className="w-full max-w-md space-y-5 bg-transparent p-8 text-center text-primary">
+      <Card className="w-full space-y-5 bg-transparent p-8 text-center text-primary">
         <H2>訂票成功！</H2>
         <H4 className="font-medium">感謝您的訂購，我們期待在活動中見到您。</H4>
         <div>
@@ -90,20 +77,46 @@ const PaymentSuccessSection = ({
           onClick={() =>
             router.push(activityId ? `/activity/${activityId}` : '/')
           }
-          className="w-full"
+          className="mx-auto max-w-md"
         >
           返回活動頁面
         </Button>
-        <Small className="mt-4">
-          {isRedirecting ? (
-            <span className="flex items-center justify-center">
-              正在跳轉回活動頁面
-              <Loader2 className="ml-2 h-4 w-4 animate-spin" />
-            </span>
-          ) : (
-            `頁面將在 ${countdown} 秒後自動跳轉回活動頁面...`
-          )}
-        </Small>
+        <Separator />
+        <H2>為您推薦</H2>
+        <section
+          id="recommend-list"
+          className="flex flex-col items-center justify-center gap-3 xl:flex-row"
+        >
+          {activities.map((activity) => {
+            return (
+              <Link key={activity._id} href={`/activity/${activity._id}`}>
+                <div className="flex flex-col gap-1">
+                  <div className="relative min-w-fit">
+                    <div className="relative h-[6.25rem] w-[12.5rem] overflow-hidden">
+                      <Image
+                        src={activity.thumbnail ?? './default.webp'}
+                        alt={
+                          activity.summary ?? 'Thumbnail default description'
+                        }
+                        placeholder="blur"
+                        blurDataURL="/loading.webp"
+                        layout="fill"
+                        objectFit="cover"
+                      />
+                      <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-2">
+                        <Small className="line-clamp-1 text-white">
+                          {activity.summary ?? '快來參加把'}
+                        </Small>
+                      </div>
+                    </div>
+                  </div>
+
+                  <H4 className="truncate">{activity.name}</H4>
+                </div>
+              </Link>
+            );
+          })}
+        </section>
       </Card>
     </>
   );
